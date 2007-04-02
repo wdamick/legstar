b94299aa5a03848a18c1e9ab6b6280c818414221
Fady <Fady@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2007-04-02 08:36:43 +0000

    Used the getHostByName API to resolve IP addresses.

--- trunk/legstar-csokrt/src/main/java/com/legstar/csok/client/CicsSocket.java
+++ trunk/legstar-csokrt/src/main/java/com/legstar/csok/client/CicsSocket.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -171,7 +172,8 @@ public class CicsSocket implements Connection {
 			mClientSocket = new Socket();
 			mClientSocket.connect(
 					new InetSocketAddress(
-							mCicsSocketEndpoint.getHostIPAddress(),
+							InetAddress.getByName(
+									mCicsSocketEndpoint.getHostIPAddress()),
 							mCicsSocketEndpoint.getHostIPPort()),
 							mConnectTimeout);
 			mClientSocket.setSoTimeout(mReceiveTimeout);
