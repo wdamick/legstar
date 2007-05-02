d74a5af1f7a12b6d09a25ecc2b438bd78cd09a73
Fady <Fady@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2007-05-02 15:42:23 +0000

    Classes to use XSLT generation do not derive from XSLTGenerator anymore, they use XSLTTransform directly. XSLTGenerator has been removed.

--- trunk/legstar-common/src/main/java/com/legstar/xslt/XSLTransform.java
+++ trunk/legstar-common/src/main/java/com/legstar/xslt/XSLTransform.java
@@ -36,7 +36,6 @@ import javax.xml.transform.sax.SAXSource;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
-import org.apache.tools.ant.BuildException;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
@@ -44,8 +43,8 @@ import org.xml.sax.XMLReader;
 import net.sf.saxon.TransformerFactoryImpl;
 
 /**
- * This class implements a single method designed to apply an XSLT transform
- * on an XML file that contains XInclude. The standard XSL engines do not
+ * This class implements methods to apply an XSLT transform on an XML file 
+ * that contains XInclude. The standard XSL engines (as of JDK 6) do not
  * resolve those XIncludes. Furthermore, since we need XSLT 2.0 transforms,
  * we need saxon until the default transform engines that come with the JDK
  * support XSLT 2.0.
@@ -54,7 +53,7 @@ import net.sf.saxon.TransformerFactoryImpl;
  *			SAXTransformerFactory stf =
  *				(SAXTransformerFactory) SAXTransformerFactory.newInstance();
  * The problem of doing this is that the default transform engine gets loaded
- * very early in the process of running this ANT task. As a consequence, all
+ * very early in the process of running ANT tasks. As a consequence, all
  * dependant classes which might be invoked from the XSLT engine (we use
  * saxon extensions) also need to be added to the classpath very early which
  * is ugly. By referencing saxon explicitly in this class, saxon can appear
@@ -66,22 +65,67 @@ import net.sf.saxon.TransformerFactoryImpl;
 public class XSLTransform {
 
 
+	/** The current XSL transformer. */
+	private Transformer mTransformer;
+	
+	/** Parameters to pass on the next transform operation. */
+	private Vector < XSLTParameter > mParams;
+	
+	/**
+	 * An XSLTransform can be created for a stylesheet. This allows
+	 * the stylesheet to be compiled only once and used multiple times.
+	 * @param xsltStream an input stream to the XSL stylesheet
+	 * @throws XSLTException if stylesheet cannot be loaded
+	 */
+	public XSLTransform(final InputStream xsltStream) throws XSLTException {
+		try {
+			StreamSource  xsltSrce = new StreamSource(xsltStream);
+			TransformerFactoryImpl stf = new TransformerFactoryImpl();
+			Templates stylesheet = stf.newTemplates(xsltSrce);
+			mTransformer = stylesheet.newTransformer();
+		} catch (TransformerConfigurationException e) {
+			throw new XSLTException(e.getMessage());
+		}
+	}
+	
+	/**
+	 * An XSLTransform can be created for a stylesheet. This allows
+	 * the stylesheet to be compiled only once and used multiple times.
+	 * @param xsltResourceName the XSL stylesheet as a resource in classpath
+	 * @throws XSLTException if stylesheet cannot be loaded
+	 */
+	public XSLTransform(final String xsltResourceName) throws XSLTException {
+		try {
+			URL xsltResource  = getClass().getResource(xsltResourceName);
+			if (xsltResource == null) {
+				throw (new XSLTException("Unable to locate resource "
+						+ xsltResourceName));
+			}
+			InputStream xsltStream = xsltResource.openStream();
+			TransformerFactoryImpl stf = new TransformerFactoryImpl();
+			Templates stylesheet = stf.newTemplates(
+					new StreamSource(xsltStream));
+			mTransformer = stylesheet.newTransformer();
+			xsltStream.close();
+		} catch (TransformerConfigurationException e) {
+			throw new XSLTException(e.getMessage());
+		} catch (IOException e) {
+			throw new XSLTException(e.getMessage());
+		}
+	}
+	
 	/**
      * Transform method.
      * 
      * @param xmlFile the XML file on which the transform should apply
-     * @param xsltStream the style sheet describing the transform as a URL
      * @param resultFile the result of the transformation process
-     * @param params the parameters passed to the style sheet
+	 * @throws XSLTException if transformation fails
      */
     public final void transform(
     		final String xmlFile,
-    		final InputStream xsltStream,
-    		final String resultFile,
-    		final Vector < XSLTParameter > params) {
+    		final String resultFile) throws XSLTException {
 
     	StreamResult  result = new StreamResult(resultFile);
-		StreamSource  xsltSrce = new StreamSource(xsltStream);
   		
 		try {
 			SAXParserFactory spf =
@@ -93,65 +137,55 @@ public class XSLTransform {
 			SAXSource xmlSrce =
 				new SAXSource(reader, new InputSource(xmlFile));
 
-			/* Make sure we have an XSLT 2.0 transformerFactory in the
-			 *  classpath */
-			TransformerFactoryImpl stf =
-				new TransformerFactoryImpl();
-			Templates stylesheet;
-			stylesheet = stf.newTemplates(xsltSrce);
-			Transformer transformer;
-			transformer = stylesheet.newTransformer();
-			
 			/* Pass on any parameters */
-			if (params != null) {
-		        for (Iterator it = params.iterator(); it.hasNext();) {
+			if (mParams != null) {
+		        for (Iterator it = mParams.iterator(); it.hasNext();) {
 		        	XSLTParameter param = (XSLTParameter) it.next();
-		            transformer.setParameter(
+		        	mTransformer.setParameter(
 		            		param.getName(), param.getExpression());
 		        }
 			}
 	        
 			/* Perform transformation */
-			transformer.transform(xmlSrce, result);
+			mTransformer.transform(xmlSrce, result);
 		} catch (SAXException e) {
 			e.printStackTrace();
-			throw (new BuildException("SAXException " + e.getMessage()));
+			throw (new XSLTException(e));
 		} catch (ParserConfigurationException e) {
 			e.printStackTrace();
-			throw (new BuildException(
-					"ParserConfigurationException " + e.getMessage()));
+			throw (new XSLTException(e));
 		} catch (TransformerConfigurationException e) {
-			e.printStackTrace();
-			throw (new BuildException(
-					"TransformerConfigurationException " + e.getMessage()));
+			throw (new XSLTException(e));
 		} catch (TransformerException e) {
-			e.printStackTrace();
-			throw (new BuildException(
-					"TransformerException " + e.getMessage()));
+			throw (new XSLTException(e));
 		}
     }
-	/**
-     * Transform method.
-     * 
-     * @param xmlFile the XML file on which the transform should apply
-     * @param xsltResource the style sheet describing the transform as a URL
-     * @param resultFile the result of the transformation process
-     * @param params the parameters passed to the style sheet
-     */
-    public final void transform(
-    		final String xmlFile,
-    		final URL xsltResource,
-    		final String resultFile,
-    		final Vector < XSLTParameter > params) {
 
-		InputStream xsltStream;
-		try {
-			xsltStream = xsltResource.openStream();
-		} catch (IOException e) {
-			e.printStackTrace();
-			throw (new BuildException(
-					"IOException " + e.getMessage()));
-		}
-		transform(xmlFile, xsltStream, resultFile, params);
-    }
+	/**
+	 * @return the XSL transformer
+	 */
+	public final Transformer getTransformer() {
+		return mTransformer;
+	}
+
+	/**
+	 * @param transformer the XSL transformer to set
+	 */
+	public final void setTransformer(final Transformer transformer) {
+		mTransformer = transformer;
+	}
+
+	/**
+	 * @return the parameters to pass on the next transform operation
+	 */
+	public final Vector < XSLTParameter > getParams() {
+		return mParams;
+	}
+
+	/**
+	 * @param params the next transform operation parameters to set
+	 */
+	public final void setParams(final Vector < XSLTParameter > params) {
+		mParams = params;
+	}
 }
