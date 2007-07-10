75c866270f3f9ef157f3d7fee27c2b81663ad155
Fady <Fady@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2007-07-10 07:40:21 +0000

    Initial check in

---
+++ legstar-coxbapi/src/main/java/com/legstar/coxb/convert/ICobolStringConverter.java
@@ -0,0 +1,87 @@
+/*******************************************************************************
+ *  LegStar legacy Web-enablement .
+ *  Copyright (C) 2007 LegSem
+ *  
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *  
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *   
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ *  02110-1301  USA
+ *  
+ *******************************************************************************/
+/**
+ * 
+ */
+package com.legstar.coxb.convert;
+
+import com.legstar.coxb.ICobolArrayStringBinding;
+import com.legstar.coxb.ICobolStringBinding;
+import com.legstar.coxb.host.HostException;
+
+/**
+ * This class uses the Strategy pattern to shield marshallers/unmarshallers from
+ * the specific Java String to cobol representation.
+ *
+ * @author Fady Moussallam
+ * 
+*/
+public interface ICobolStringConverter {
+	/**
+	 * Converts an element java value to a host representation stored in host
+	 * buffer.
+	 * @param ce Cobol element descriptor 
+	 * @param hostTarget Target host buffer
+	 * @param offset Offset in the target host buffer
+	 * @return the new offset after host buffer has been updated
+	 * @throws HostException if conversion fails
+	 */
+	int toHost(ICobolStringBinding ce, byte[] hostTarget, int offset)
+		throws HostException; 
+	
+	/**
+	 * Converts an array of java values to a host representation stored in 
+	 * host buffer.
+	 * @param ce Cobol array descriptor 
+	 * @param hostTarget Target host buffer
+	 * @param offset Offset in the target host buffer
+	 * @param currentOccurs actual number of items in array
+	 * @return the new offset after host buffer has been updated
+	 * @throws HostException if conversion fails
+	 */
+	int toHost(ICobolArrayStringBinding ce, byte[] hostTarget, int offset,
+			int currentOccurs)
+		throws HostException;
+	
+	/**
+	 * Converts an element Cobol value from a host buffer to a java value.
+	 * @param ce Cobol element descriptor 
+	 * @param hostSource Source host buffer
+	 * @param offset Offset in the source host buffer
+	 * @return the new offset after host buffer has been read
+	 * @throws HostException if conversion fails
+	 */
+	int fromHost(ICobolStringBinding ce, byte[] hostSource, int offset)
+		throws HostException;
+	
+	/**
+	 * Converts an array of Cobol values from a host buffer to a java array.
+	 * @param ce Cobol array descriptor 
+	 * @param hostSource Source host buffer
+	 * @param offset Offset in the source host buffer
+	 * @param currentOccurs actual number of items in array
+	 * @return the new offset after host buffer has been read
+	 * @throws HostException if conversion fails
+	 */
+	int fromHost(ICobolArrayStringBinding ce, byte[] hostSource, int offset,
+			int currentOccurs)
+		throws HostException;
+}
