72401a20e3195fff43c7eb7502fee8323dc533fc
Fady <Fady@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2007-08-14 13:58:32 +0000

    Added a specific test for lsfileae over MQ

--- trunk/legstar-cixsgen-clients/src/test/java/com/legstar/clients/tests/AllTests.java
+++ trunk/legstar-cixsgen-clients/src/test/java/com/legstar/clients/tests/AllTests.java
@@ -8,29 +8,30 @@ public class AllTests {
 	public static Test suite() {
 		TestSuite suite = new TestSuite("Test for com.legstar.clients.tests");
 		//$JUnit-BEGIN$
-		suite.addTestSuite(ClientredmultiTest.class);
 		suite.addTestSuite(ClientlsfileadTest.class);
-		suite.addTestSuite(ClientbinarchtTest.class);
-		suite.addTestSuite(ClientbinpkdusTest.class);
 		suite.addTestSuite(ClientfixarcomTest.class);
 		suite.addTestSuite(ClientdplarchtIterationTest.class);
-		suite.addTestSuite(ClientredoperaTest.class);
-		suite.addTestSuite(ClientlsfilealTest.class);
-		suite.addTestSuite(ClientbinnatusTest.class);
 		suite.addTestSuite(ClientfloatmixTest.class);
 		suite.addTestSuite(ClientlsfileacTest.class);
-		suite.addTestSuite(ClientlsfileaeIterationTest.class);
 		suite.addTestSuite(ClientredinoutTest.class);
-		suite.addTestSuite(ClientredsimptTest.class);
 		suite.addTestSuite(ClientredbothaTest.class);
-		suite.addTestSuite(ClientdoublmixTest.class);
+		suite.addTestSuite(ClientredsimptTest.class);
 		suite.addTestSuite(ClientfixarsimTest.class);
 		suite.addTestSuite(ClientvararcomTest.class);
-		suite.addTestSuite(ClientdplarchtTest.class);
-		suite.addTestSuite(ClienttypesmixTest.class);
+		suite.addTestSuite(ClientNumzonedTest.class);
 		suite.addTestSuite(ClientlsfileaeTest.class);
 		suite.addTestSuite(ClientfixarnumTest.class);
 		suite.addTestSuite(ClientbinnatsiTest.class);
+		suite.addTestSuite(ClientredmultiTest.class);
+		suite.addTestSuite(ClientbinarchtTest.class);
+		suite.addTestSuite(ClientbinpkdusTest.class);
+		suite.addTestSuite(ClientredoperaTest.class);
+		suite.addTestSuite(ClientlsfilealTest.class);
+		suite.addTestSuite(ClientbinnatusTest.class);
+		suite.addTestSuite(ClientlsfileaeIterationTest.class);
+		suite.addTestSuite(ClientdoublmixTest.class);
+		suite.addTestSuite(ClientdplarchtTest.class);
+		suite.addTestSuite(ClienttypesmixTest.class);
 		//$JUnit-END$
 		return suite;
 	}
