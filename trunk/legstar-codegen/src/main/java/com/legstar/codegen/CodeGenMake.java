0ec9ec3508837afd2a379987b11c0a9174d496f7
Fady <Fady@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2008-02-24 15:59:59 +0000

    Pointing to the right SVN location

---
+++ trunk/legstar-codegen/src/main/java/com/legstar/codegen/CodeGenMake.java
@@ -0,0 +1,307 @@
+/*******************************************************************************
+ *  LegStar legacy Web-enablement .
+ *  Copyright (C) 2007 LegSem
+ *  
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *  
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *   
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ *  02110-1301  USA
+ *  
+ *******************************************************************************/
+package com.legstar.codegen;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+
+/**
+ * This Ant task iteratively calls the Velocity template engine to create
+ * a list of artifacts described in an xml file.
+ * The xml file typically describes a target as a sequential list of steps.
+ * Each step is associated with a velocity template and produces an artifact.
+ * TODO describe the format of xml make in an xsd
+ */
+public class CodeGenMake extends Task {
+
+    /** The name of the calling generator. */
+	private String mGeneratorName;
+	
+	/** The cixs make file name. */
+    private String mCodeGenMakeFileName;
+    
+    /** Velocity templates are applied against a model. 
+     * This is the model name. */
+    private String mModelName;
+    
+    /** Velocity templates are applied against a model. 
+     * This is the model itself. */
+    private Object mModel;
+    
+    /** XML element representing a code generation target. */
+    public static final String CODEGEN_TARGET_XML_E = "target";
+
+    /** XML attribute representing a code generation target name. */
+    public static final String CODEGEN_TARGET_NAME_XML_A = "name";
+
+    /** XML attribute representing a code generation target directory. */
+    public static final String CODEGEN_TARGET_DIR_XML_A = "dir";
+
+    /** XML element representing a code generation step with a template. */
+    public static final String CODEGEN_STEP_XML_E = "step";
+    
+    /** XML attribute representing a code generation template name. */
+    public static final String CODEGEN_TEMPLATE_NAME_XML_A = "templateName";
+
+    /** XML attribute representing a code generation target file. */
+    public static final String CODEGEN_TEMPLATE_TARGET_FILE_XML_A = "targetFile";
+    
+    
+    /** Logger. */
+	private static final Log LOG = LogFactory.getLog(CodeGenMake.class);
+
+    /** @{inheritDoc}*/
+    @Override
+   public final void init() {
+        try {
+            LOG.info("Initializing Code generation make processor");
+            CodeGenUtil.initVelocity();
+        } catch (Exception e) {
+            throw new BuildException(e.getMessage());
+        }
+    }
+    
+    /** @{inheritDoc}*/
+    @Override
+    public final void execute() {
+        long start = System.currentTimeMillis();
+        try {
+            LOG.debug("Start make processor");
+            Document doc = getInput();
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Make file name   = " + mCodeGenMakeFileName);
+                LOG.debug("Model name       = " + mModelName);
+            }
+            processTargets(doc);
+        } catch (CodeGenMakeException e) {
+            LOG.error("Make processor failure", e);
+            throw new BuildException(e.getMessage());
+        }
+        long end = System.currentTimeMillis();
+        LOG.info("Make processing success for " + mCodeGenMakeFileName);
+        LOG.info("Duration = " + (end - start) + " ms");
+    }
+
+    /**
+     * Check input values and load the xml make file.
+     * @return an in-memory DOM xml make
+     * @throws CodeGenMakeException if initialization fails
+     */
+    private Document getInput() throws CodeGenMakeException {
+        if (mCodeGenMakeFileName == null || mCodeGenMakeFileName.length() == 0) {
+            throw new CodeGenMakeException("Missing make file parameter");
+        }
+        if (mModelName == null || mModelName.length() == 0) {
+            throw new CodeGenMakeException("Missing model name parameter");
+        }
+        if (mModel == null) {
+            throw new CodeGenMakeException("Missing model parameter");
+        }
+        File makeFile = new File(mCodeGenMakeFileName);
+        
+        if (!makeFile.exists()) {
+            throw new CodeGenMakeException("Code generation make file "
+                    + makeFile + " does not exist");
+        }
+        return load(makeFile);
+    }
+    
+    /**
+     * Process targets sequentially, invoking the velocity engine for each
+     * target.
+     * @param doc a valid DOM for a cixs make
+     * @throws CodeGenMakeException if processing fails
+     */
+    private void processTargets(final Document doc) throws CodeGenMakeException {
+        NodeList listOfElements = doc.getElementsByTagName(
+                CODEGEN_TARGET_XML_E);
+        if (listOfElements == null || listOfElements.getLength() == 0) {
+            throw (new CodeGenMakeException(
+                    "Empty or invalid code generation make file"));
+        }
+        Element targetElement = (Element) listOfElements.item(0);
+        String targetName = targetElement.getAttribute(CODEGEN_TARGET_NAME_XML_A);
+        if (targetName == null || targetName.length() == 0) {
+            throw new CodeGenMakeException("Missing name attribute for "
+                    + CODEGEN_TARGET_XML_E + " element");
+        }
+        String targetDir = targetElement.getAttribute(CODEGEN_TARGET_DIR_XML_A);
+        if (targetDir != null && targetDir.length() > 0) {
+            try {
+                CodeGenUtil.checkDirectory(targetDir, true);
+            } catch (IllegalArgumentException e) {
+                throw new CodeGenMakeException(e);
+            }
+        }
+        listOfElements = targetElement.getElementsByTagName(
+                CODEGEN_STEP_XML_E);
+        
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Processing target");
+            LOG.debug("Target name      = " + targetName);
+            LOG.debug("Target directory = " + targetDir);
+        }
+        for (int i = 0; i < listOfElements.getLength(); i++) {
+            Element templateEl = (Element) listOfElements.item(i);
+            String templateName = templateEl.getAttribute(
+                    CODEGEN_TEMPLATE_NAME_XML_A);
+            if (templateName == null || templateName.length() == 0) {
+                throw new CodeGenMakeException(
+                        "Missing template name attribute for "
+                        + CODEGEN_STEP_XML_E + " element");
+            }
+            String templateTargetFileName = templateEl.getAttribute(
+                    CODEGEN_TEMPLATE_TARGET_FILE_XML_A);
+            if (templateTargetFileName == null
+                    || templateTargetFileName.length() == 0) {
+                throw new CodeGenMakeException(
+                        "Missing template target file name attribute for "
+                        + CODEGEN_STEP_XML_E + " element");
+            }
+            CodeGenUtil.processTemplate(
+            		mGeneratorName,
+            		templateName,
+            		mModelName, mModel,
+            		getParameters(templateEl),
+                    CodeGenUtil.getFile(targetDir, templateTargetFileName));
+        }
+    }
+    
+    /**
+     * Template elements might have children. These are used to pass additional
+     * parameters to the template generation process.
+     * Children are expected to have a single value attribute otherwise, they
+     * are ignored.
+     * @param templateEl the current template element
+     * @return a Map of parameters/values
+     */
+    private Map<String, Object> getParameters(final Element templateEl) {
+        Map<String, Object> parameters = new HashMap <String, Object>();
+        NodeList childs = templateEl.getChildNodes();
+        for (int i = 0; i < childs.getLength(); i++) {
+            if (childs.item(i) instanceof Element) {
+                Element parmEl = (Element) childs.item(i);
+                String value = parmEl.getAttribute("value");
+                if (value != null) {
+                    parameters.put(parmEl.getNodeName(), value);
+                }
+            }
+        }
+        return parameters;
+    }
+    
+    /**
+     * Loads code generation make file from a serialized XML.
+     * @param makeFile the serialized file
+     * @return an in-memory DOM code generation make
+     * @throws CodeGenMakeException if load fails
+     */
+    public final Document load(final File makeFile) throws CodeGenMakeException {
+        DocumentBuilderFactory docBuilderFactory =
+            DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder;
+        try {
+            docBuilderFactory.setNamespaceAware(false);
+            docBuilder = docBuilderFactory.newDocumentBuilder();
+            Document doc = docBuilder.parse(makeFile);
+            return doc;
+        } catch (ParserConfigurationException e) {
+            throw (new CodeGenMakeException(e));
+        } catch (SAXException e) {
+            throw (new CodeGenMakeException(e));
+        } catch (IOException e) {
+            throw (new CodeGenMakeException(e));
+        }
+    }
+    
+    /**
+     * @return the code generation Make File name
+     */
+    public final String getCodeGenMakeFileName() {
+        return mCodeGenMakeFileName;
+    }
+    
+    /**
+     * @param codeGenMakeFileName the code generation Make File name to set
+     */
+    public final void setCodeGenMakeFileName(final String codeGenMakeFileName) {
+        mCodeGenMakeFileName = codeGenMakeFileName;
+    }
+
+    /**
+     * @return the model name
+     */
+    public final String getModelName() {
+        return mModelName;
+    }
+
+    /**
+     * @param modelName the model name to set
+     */
+    public final void setModelName(final String modelName) {
+        mModelName = modelName;
+    }
+
+    /**
+     * @return the model used to apply templates to
+     */
+    public final Object getModel() {
+        return mModel;
+    }
+
+    /**
+     * @param model the model to set
+     */
+    public final void setModel(final Object model) {
+        mModel = model;
+    }
+
+	/**
+	 * @return the name of the calling generator
+	 */
+	public final String getGeneratorName() {
+		return mGeneratorName;
+	}
+
+	/**
+	 * @param generatorName the name of the calling generator to set
+	 */
+	public final void setGeneratorName(final String generatorName) {
+		mGeneratorName = generatorName;
+	}
+
+}
