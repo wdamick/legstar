486129ce5f935c05fc5acbb4a0e514cfdcf95454
Fady <Fady@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2008-06-06 10:32:55 +0000

    Uses the ant launcher model and share code with common-plugin.

--- legstar-coxbgen-plugin/src/main/java/com/legstar/eclipse/plugin/coxbgen/wizards/CoxbGenWizard.java
+++ legstar-coxbgen-plugin/src/main/java/com/legstar/eclipse/plugin/coxbgen/wizards/CoxbGenWizard.java
@@ -19,111 +19,42 @@
  *  
  *******************************************************************************/
 package com.legstar.eclipse.plugin.coxbgen.wizards;
-import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.eclipse.jface.operation.IRunnableWithProgress;
-import org.eclipse.jface.resource.ImageDescriptor;
 
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
 import java.lang.reflect.InvocationTargetException;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IWorkspaceRoot;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.Path;
 
 import com.legstar.eclipse.plugin.coxbgen.Activator;
-import com.legstar.eclipse.plugin.common.LegstarAntRunner;
-import com.legstar.eclipse.plugin.common.LegstarReport;
-import com.legstar.eclipse.plugin.coxbgen.CoxbgenPreferences;
-
-import java.util.Map;
-import java.util.HashMap;
+import com.legstar.eclipse.plugin.common.wizards.AbstractWizard;
 
 
 /**
- * This wizard role is to create annotated JAXB classes from an annotated
+ * This wizard role is to create annotated JAXB classes from a COBOL-annotated
  * XML schema and then generate binding classes.
  */
 
-public class CoxbGenWizard extends Wizard implements INewWizard {
+public class CoxbGenWizard extends AbstractWizard {
 	
 	/** The main page of controls. */
-	private CoxbGenWizardPage page;
+	private CoxbGenWizardPage mCoxbGenPage;
 	
 	/** The current workbench selection. */
-	private ISelection selection;
+	private ISelection mInitialSelection;
 	
 	/** The current XML schema file. */
 	private IFile mXsdFile = null;
-
-	/** Set of common preferences. */
-	private CoxbgenPreferences mCoxbgenPref;
-	
-	/** Relative path to JAXB classes generation ant script. */
-	private static final String JAXB_XSL_LOC = "/ant/build-jaxb.xml";
-	
-	/** Relative path to COXB classes generation ant script. */
-	private static final String COXB_XSL_LOC = "/ant/build-coxb.xml";
-	
-	/** Target for JAXB generation (compilation is necessary because COXB
-	 * will perform reflection on JAXB generated classes). */
-	private static final String JAXB_TARGET = "compileJAXB";
-	
-	/** Target for COXB generation (avoid compilation). */
-	private static final String COXB_TARGET = "generateCOXB";
-	
-	/** Error message if Coxbgen location has not been set. */
-	private static final String GEN_NOT_SET = "Coxbgen location not set in "
-		+ "preferences. Use Window-->Preferences to set the LegStar COXB "
-		+ "Generator location to a valid Coxbgen install folder (Coxbgen must "
-		+ "have been installed prior to using this plugin).";
-	
-	/** Page image location. */
-	private static final String PAGE_IMG = "icons/legsemlogo.jpg";
-	
-	/** Error messages dialog boxes titles. */
-	private static final String ERROR_TITLE = "Coxbgen Error";
-	
-	/** Monitor text when JAXB generation starts. */
-	private static final String MONITOR_JAXB_START =
-		"Generating JAXB classes for ";
-	
-	/** Monitor text when COXB generation starts. */
-	private static final String MONITOR_COXB_START =
-		"Generating COXB classes for ";
-	
-	/** First part of error message for non existent container. */
-	private static final String NO_CONTAINER_PART1 = "Container \"";
-	
-	/** Second part of error message for non existent container. */
-	private static final String NO_CONTAINER_PART2 =
-		"\" does not exist.";
-	
-	/** Ant script XML Schema file name. */
-	private static final String ANT_XSD_FILE = "xsd.file";
-
-	/** Ant script JAXB Sources target location property name. */
-	private static final String ANT_JAXB_SRC_DIR = "jaxb.src.dir";
-
-	/** Ant script JAXB binaries target location property name. */
-	private static final String ANT_JAXB_BIN_DIR = "jaxb.bin.dir";
 
 	/**
 	 * Constructor for CoxbGenWizard.
 	 * @throws CoreException if initialization goes wrong 
 	 */
 	public CoxbGenWizard() throws CoreException {
-		super();
-		initialize();
-		mXsdFile = null;
+		this(null);
 	}
 	
 	/**
@@ -133,157 +64,35 @@ public class CoxbGenWizard extends Wizard implements INewWizard {
 	 */
 	public CoxbGenWizard(final IFile xsdFile) throws CoreException {
 		super();
-		initialize();
+        setNeedsProgressMonitor(true);
 		mXsdFile = xsdFile;
 	}
 	
-	/**
-	 * Setup Wizard environment.
-	 * @throws CoreException if setup fails
-	 */
-	private void initialize() throws CoreException {
-		setNeedsProgressMonitor(true);
-		ImageDescriptor image =
-            AbstractUIPlugin.
-                imageDescriptorFromPlugin(Activator.PLUGIN_ID, PAGE_IMG);
-        setDefaultPageImageDescriptor(image);
-        mCoxbgenPref = new CoxbgenPreferences();
-	}
-	
-	/**
-	 * Adding the page to the wizard.
-	 */
-
-	public final void addPages() {
-		page = new CoxbGenWizardPage(selection, mXsdFile);
-		addPage(page);
-	}
-
 	/**
 	 * This method is called when 'Finish' button is pressed in the wizard.
 	 * We will create an operation and run it using wizard as execution context.
 	 * @return true if processing went fine
 	 */
 	public final boolean performFinish() {
-		final IFile xsdFile = page.getXsdFile();
-		final String [] rootNames = page.getRootName();
-		final String srcdirName = page.getSrcDirName();
-		final String bindirName = page.getBinDirName();
-		final String jaxbPackageName = page.getJaxbPackageName();
-		final String coxbgenDir = mCoxbgenPref.getCoxbgenLocation();
 		
-		if (coxbgenDir == null || coxbgenDir.length() == 0) {
-			MessageDialog.openError(getShell(),
-					ERROR_TITLE, GEN_NOT_SET);
-			LegstarReport.logCoreError(GEN_NOT_SET,
-					Activator.PLUGIN_ID);
-			return false;
-		}
-		
-		IRunnableWithProgress op = new IRunnableWithProgress() {
-			public void run(final IProgressMonitor monitor)
-				throws InvocationTargetException {
-				try {
-					doFinish(xsdFile, rootNames, srcdirName,
-							bindirName, jaxbPackageName, monitor);
-				} catch (CoreException e) {
-					throw new InvocationTargetException(e);
-				} finally {
-					monitor.done();
-				}
-			}
-		};
 		try {
-			getContainer().run(true, false, op);
+			IRunnableWithProgress op = new CoxbGenWizardRunnable(mCoxbGenPage);
+			getContainer().run(true, true, op);
 		} catch (InterruptedException e) {
 			return false;
 		} catch (InvocationTargetException e) {
-			Throwable realException = e.getTargetException();
-			LegstarReport.logCoreException(realException, Activator.PLUGIN_ID);
-			MessageDialog.openError(getShell(),
-					ERROR_TITLE, realException.getClass().toString() + " "
-					+ realException.getMessage());
-			return false;
+            errorDialog(getShell(), "Generation error", Activator.PLUGIN_ID,
+                    "Binding classes generation failed ",
+                    "The generation attempt for " 
+                    + mXsdFile.getName()
+                    + " has generated a InvocationTargetException "
+                    + e.getTargetException());
+            logCoreException(e.getTargetException(), Activator.PLUGIN_ID);
+            return false;
 		}
 		return true;
 	}
 	
-	/**
-	 * The worker method. It will collect the bialog data and create an
-	 * ebvironment for an ANT script to generate the target classes.
-	 * @param xsdFile The XML schema file
-	 * @param rootNames the root type names
-	 * @param srcdirName the target source directory
-	 * @param bindirName the target binary directory
-	 * @param jaxbPackageName the target jaxb package name
-	 * @param monitor to follow progress
-	 * @throws CoreException if something goes wrong
-	 */
-	private void doFinish(
-		final IFile xsdFile,
-		final String [] rootNames,
-		final String srcdirName,
-		final String bindirName,
-		final String jaxbPackageName,
-		final IProgressMonitor monitor)
-		throws CoreException {
-		
-		int scale = 1000;
-		monitor.beginTask(MONITOR_JAXB_START
-				+ xsdFile.getName(), (1 + rootNames.length) * scale);
-		LegstarAntRunner antRunner = new LegstarAntRunner();
-		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-		
-		/* Check that we have a valid source container to store our files */
-		IResource resource = root.findMember(new Path(srcdirName));
-		if (!resource.exists() || !(resource instanceof IContainer)) {
-			LegstarReport.throwCoreException(NO_CONTAINER_PART1 + srcdirName
-					+ NO_CONTAINER_PART2, Activator.PLUGIN_ID);
-		}
-		IContainer srcdir = (IContainer) resource;
-
-		/* Check that we have a valid binary container to store our files */
-		resource = root.findMember(new Path(bindirName));
-		if (!resource.exists() || !(resource instanceof IContainer)) {
-			LegstarReport.throwCoreException(NO_CONTAINER_PART1 + bindirName
-					+ NO_CONTAINER_PART2, Activator.PLUGIN_ID);
-		}
-		IContainer bindir = (IContainer) resource;
-
-		/* Parameters expected by ANT task */
-		Map < String, String > properties = new HashMap < String, String >();
-		properties.put(ANT_XSD_FILE, xsdFile.getLocation().toOSString());
-		properties.put(ANT_JAXB_SRC_DIR, srcdir.getLocation().toOSString());
-		properties.put(ANT_JAXB_BIN_DIR, bindir.getLocation().toOSString());
-		
-		/* Generate the JAXB classes */
-		String[] jaxbTargets = {JAXB_TARGET};
-		antRunner.run(
-				mCoxbgenPref.getCoxbgenLocation() + JAXB_XSL_LOC,
-				jaxbTargets,
-				properties,
-				monitor,
-				scale);
-		
-		/* Generate the binding classes */
-		monitor.setTaskName(MONITOR_COXB_START	+ xsdFile.getName());
-		
-		for (int i = 0; i < rootNames.length; i++) {
-			String[] coxbTargets = {COXB_TARGET};
-			properties.put("jaxb.root.name", rootNames[i]);
-			antRunner.run(mCoxbgenPref.getCoxbgenLocation() + COXB_XSL_LOC,
-					coxbTargets,
-					properties,
-					monitor,
-					scale);
-		}
-		
-		/* Make sure the project is refreshed as the files were created outside
-		 * the Eclipse API. */
-        srcdir.refreshLocal(
-            IResource.DEPTH_INFINITE, monitor);
-
-	}
 	/**
 	 * We will accept the selection in the workbench to see if
 	 * we can initialize from it.
@@ -294,8 +103,17 @@ public class CoxbGenWizard extends Wizard implements INewWizard {
 	 */
 	public final void init(
 			final IWorkbench workbench,
-			final IStructuredSelection sel) {
-		this.selection = sel;
+			final IStructuredSelection selection) {
+		mInitialSelection = selection;
 	}
 	
+	/**
+	 * Adding the page to the wizard.
+	 */
+
+	public final void addPages() {
+		mCoxbGenPage = new CoxbGenWizardPage(mInitialSelection, mXsdFile);
+		addPage(mCoxbGenPage);
+	}
+
 }
---
+++ legstar-coxbgen-plugin/src/main/java/com/legstar/eclipse/plugin/coxbgen/wizards/CoxbGenWizardRunnable.java
@@ -0,0 +1,137 @@
+package com.legstar.eclipse.plugin.coxbgen.wizards;
+
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+import com.legstar.codegen.models.AbstractAntBuildModel;
+import com.legstar.coxb.gen.CoxbGenModel;
+import com.legstar.eclipse.plugin.common.wizards.AbstractWizardRunnable;
+
+/**
+ * Background task that performs the actual binding generation. The process
+ * involves 2 steps:
+ * <ul>
+ *  <li>Build an ant script file using a velocity template</li>
+ *  <li>Launch the ant script as a background process</li>
+ * </ul>
+ */
+public class CoxbGenWizardRunnable extends AbstractWizardRunnable {
+	
+	/**
+	 * Where the default Jaxb binding files are stored.
+	 * TODO turn this into a parameter.
+	 */
+	private static final String XJB_SUB_FOLDER = "/lib";
+	
+	/** Part of the ant script file name generated. Allows segregating
+	 * this ant file from the ones produced by other LegStar wizards. */
+	private static final String ANT_FILE_NAME_ID = "coxb-";
+
+	/** Source Xsd file name. */
+	private String mXsdFileName;
+	
+	/** The JAXB suffix optionally appended to Xsd complex type names to derive
+	 *  a JAXB class name. */
+	private String mJaxbTypeNameSuffix;
+	
+	/** Monitor text when COXB generation starts. */
+	private static final String MONITOR_COXB_GENERATION_START =
+		"Generating ANT script for binding classes generation";
+
+	/**
+	 * Constructs the backend generation task. 
+	 * The ant script will be generated under the folder from
+	 * preferences.
+	 * TODO allow user to select the ant script location
+	 * @param coxbGenWizardPage the main wizard page
+	 * @throws InvocationTargetException if construction fails
+	 */
+	public CoxbGenWizardRunnable(
+			final CoxbGenWizardPage coxbGenWizardPage)
+			throws InvocationTargetException {
+		super(null,
+				coxbGenWizardPage.getSrcDirRelativePathName(),
+				getAntScriptFileName(coxbGenWizardPage.getXsdFile().getName()));
+        mXsdFileName = coxbGenWizardPage.getXsdFile().getName();
+        mJaxbTypeNameSuffix = coxbGenWizardPage.getJaxbTypeNameSuffix();
+        setAntBuildModel(getModel(coxbGenWizardPage));
+	}
+
+	/** {@inheritDoc} */
+	public void run(
+			final IProgressMonitor monitor) throws InvocationTargetException,
+			InterruptedException {
+		int scale = 1;
+		/* 3 tasks because launcher counts as a hidden one*/
+		monitor.beginTask(MONITOR_COXB_GENERATION_START + mXsdFileName,
+				3 * scale);
+		try {
+			/* 1. Create the ant build */
+			createBuild(monitor, scale);
+
+			/* 2 & 3. Execute the generated build.*/
+			runBuild(monitor, scale);
+			
+		} finally {
+			monitor.done();
+		}
+	}
+
+    /**
+     * Create a model ready to be passed to velocity for ant script generation.
+     * @param coxbGenWizardPage the wizard page holding input parameters
+     * @return a valid model
+     * @throws InvocationTargetException if model cannot be built
+     */
+    protected CoxbGenModel getModel(
+            final CoxbGenWizardPage coxbGenWizardPage)
+            throws InvocationTargetException {
+    	CoxbGenModel model = new CoxbGenModel();
+        model.setProductLocation(getPluginInstallLocation());
+        model.setXsdFile(new File(
+        		coxbGenWizardPage.getXsdFile().getLocation().toOSString()));
+        /* TODO add fields to support different src/bin dirs for Jaxb & Coxb */
+		model.setJaxbSrcDir(new File(
+				getPathName(coxbGenWizardPage.getSrcDirRelativePathName())));
+		model.setJaxbBinDir(new File(
+				getPathName(coxbGenWizardPage.getBinDirRelativePathName())));
+		model.setJaxbXjcBindingDir(new File(
+				model.getProductLocation() + XJB_SUB_FOLDER));
+		model.setCoxbSrcDir(new File(
+				getPathName(coxbGenWizardPage.getSrcDirRelativePathName())));
+		model.setCoxbBinDir(new File(
+				getPathName(coxbGenWizardPage.getBinDirRelativePathName())));
+        model.setJaxbRootClassNames(coxbGenWizardPage.getJaxbRootClassNames());
+        model.setJaxbTypeNameSuffix(mJaxbTypeNameSuffix);
+        return model;
+    }
+    
+    /**
+     * Construct a full path name from a relative one.
+     * @param relativePathName a pathname relative to the workspace root
+     * @return a full path name
+     */
+    private static String getPathName(final String relativePathName) {
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		IContainer container = (IContainer) root.findMember(relativePathName);
+		return container.getLocation().toOSString();
+	}
+    
+	/**
+	 * Generate a valid ant script file name.
+	 * @param xsdFileName the source xsd file name.
+	 * @return the ant script file name
+	 */
+	private static String getAntScriptFileName(final String xsdFileName) {
+		return AbstractAntBuildModel.ANT_FILE_PREFIX
+		+ ANT_FILE_NAME_ID
+		+ xsdFileName + '.'
+		+ AbstractAntBuildModel.ANT_FILE_SUFFIX;	
+	}
+
+}
