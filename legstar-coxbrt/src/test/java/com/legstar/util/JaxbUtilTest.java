8a7654c628357a01280b0a75b0c7713fa0bf5e23
Fady <Fady@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2008-06-06 10:32:04 +0000

    The utility class now has a method to extract a POJO class from a JAXB class. special annotations within the JAXB class gives the hidden POJO class name.

---
+++ legstar-coxbrt/src/test/java/com/legstar/util/JaxbUtilTest.java
@@ -0,0 +1,20 @@
+package com.legstar.util;
+
+import junit.framework.TestCase;
+
+public class JaxbUtilTest extends TestCase {
+	
+	public void testGetJavaClassNameAnnotations() throws Exception {
+		assertEquals("com.legstar.xsdc.test.cases.jvmquery.JVMQueryReply",
+				JaxbUtil.getJavaClassName(
+				"com.legstar.test.coxb.jvmquery", "JvmQueryReply"));
+		
+	}
+
+	public void testGetJavaClassNameNoAnnotations() throws Exception {
+		assertEquals("com.legstar.test.coxb.dplarcht.DfhcommareaType",
+				JaxbUtil.getJavaClassName(
+				"com.legstar.test.coxb.dplarcht", "DfhcommareaType"));
+		
+	}
+}
