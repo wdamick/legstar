c409f70086677bbbda08e69449c44bba9c686e93
Fady <Fady@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2008-06-10 09:57:52 +0000

    Added host character set as a shared preference for all plugins.

--- trunk/legstar-common-plugin/src/main/java/com/legstar/eclipse/plugin/common/preferences/PreferenceConstants.java
+++ trunk/legstar-common-plugin/src/main/java/com/legstar/eclipse/plugin/common/preferences/PreferenceConstants.java
@@ -8,4 +8,6 @@ public class PreferenceConstants {
 	/** Folder where ant scripts should be generated. */
     public static final String ANT_SCRIPTS_FOLDER = "antScriptsFolder";
     
+	/** Mainframe character set. @see java.nio.charset.Charset */
+    public static final String HOST_CHARSET = "hostCharset";
 }
