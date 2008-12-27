e605a91c8f919a0366d5ece334fd6bba8625a270
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2008-12-27 11:19:51 +0000

    Cleaning up (checkstyle) the test code.

--- trunk/legstar-engine/src/test/java/com/legstar/host/server/ConfigReaderTest.java
+++ trunk/legstar-engine/src/test/java/com/legstar/host/server/ConfigReaderTest.java
@@ -19,62 +19,71 @@ import org.apache.commons.configuration.tree.xpath.XPathExpressionEngine;
 
 import junit.framework.TestCase;
 
+/**
+ * Test the configuration reader.
+ *
+ */
 public class ConfigReaderTest extends TestCase {
-	
-	private static final String HOST_ENDPOINT_CFG = "hostEndPoints.hostEndPoint";
-	
-	@SuppressWarnings("unchecked")
-	public void testReadConfig() throws ConfigurationException {
-		HierarchicalConfiguration config = Util.getCombinedConfiguration();
-		List endpoints = config.configurationsAt(HOST_ENDPOINT_CFG);
-		for(Iterator it = endpoints.iterator(); it.hasNext();)	{
-			HierarchicalConfiguration sub = (HierarchicalConfiguration) it.next();
-			System.out.println("EndPoint name=" + sub.getString("[@name]"));
-			System.out.println("EndPoint ipaddress=" + sub.getString("hostIPAddress"));
-			
-		}
-		
-	}
 
-	@SuppressWarnings("unchecked")
-	public void testReadConfigXPath() throws ConfigurationException {
-		HierarchicalConfiguration config = Util.getCombinedConfiguration();
-		config.setExpressionEngine(new XPathExpressionEngine());
-		
-		List endpoints = config.configurationsAt("hostEndPoints/hostEndPoint[@name = 'TheOtherMainframe']");
-		assertTrue(!endpoints.isEmpty());
-		for(Iterator it = endpoints.iterator(); it.hasNext();)	{
-			HierarchicalConfiguration sub = (HierarchicalConfiguration) it.next();
-			System.out.println("EndPoint name=" + sub.getString("@name"));
-			System.out.println("EndPoint ipaddress=" + sub.getString("hostIPAddress"));
-			
-		}
-	}
-	
-	@SuppressWarnings("unchecked")
-	public void testNotFoundElement() throws ConfigurationException {
-		HierarchicalConfiguration config = Util.getCombinedConfiguration();
-		config.setExpressionEngine(new XPathExpressionEngine());
-		
-		List endpoints = config.configurationsAt("hostEndPoints/hostEndPoint[@name = 'TheOtterrMainframe']");
-		if (endpoints == null || endpoints.size() == 0) {
-			System.out.println("The requested endpoint:" 
-			+ "TheOtterrMainframe"
-			+ " is not defined in " + Util.CONFIG_FILE);
-		}
-		assertTrue(endpoints.isEmpty());
-	}
+    /** Endpoint configuration. */
+    private static final String HOST_ENDPOINT_CFG = "hostEndPoints.hostEndPoint";
 
-	@SuppressWarnings("unchecked")
-	public void testReadConfigXPathDirect() throws ConfigurationException {
-		HierarchicalConfiguration config = Util.getCombinedConfiguration();
-		config.setExpressionEngine(new XPathExpressionEngine());
-		
-		List  endpoints = config.configurationsAt("hostEndPoints/hostEndPoint[@name = 'TheOtherMainframe']");
-		assertTrue(!endpoints.isEmpty());
-		HierarchicalConfiguration endpoint = (HierarchicalConfiguration) endpoints.get(0);
-		System.out.println("EndPoint name=" + endpoint.getString("@name"));
-		System.out.println("EndPoint ipaddress=" + endpoint.getString("hostIPAddress"));
-	}
-	
+    /**
+     * Basic configuration read.
+     * @throws ConfigurationException if read fails
+     */
+    public void testReadConfig() throws ConfigurationException {
+        HierarchicalConfiguration config = Util.getCombinedConfiguration();
+        List < ? > endpoints = config.configurationsAt(HOST_ENDPOINT_CFG);
+        for (Iterator < ? > it = endpoints.iterator(); it.hasNext();) {
+            HierarchicalConfiguration sub = (HierarchicalConfiguration) it.next();
+            assertEquals("TheOtherMainframe", sub.getString("[@name]"));
+            assertEquals("mainframe", sub.getString("hostIPAddress"));
+        }
+    }
+
+    /**
+     * Read using XPath expression.
+     * @throws ConfigurationException if read fails
+     */
+    public void testReadConfigXPath() throws ConfigurationException {
+        HierarchicalConfiguration config = Util.getCombinedConfiguration();
+        config.setExpressionEngine(new XPathExpressionEngine());
+
+        List < ? > endpoints = config.configurationsAt("hostEndPoints/hostEndPoint[@name = 'TheOtherMainframe']");
+        assertTrue(!endpoints.isEmpty());
+        for (Iterator < ? > it = endpoints.iterator(); it.hasNext();) {
+            HierarchicalConfiguration sub = (HierarchicalConfiguration) it.next();
+            assertEquals("TheOtherMainframe", sub.getString("@name"));
+            assertEquals("mainframe", sub.getString("hostIPAddress"));
+        }
+    }
+
+    /**
+     * Test reading a non existing element.
+     * @throws ConfigurationException if test fails
+     */
+    public void testNotFoundElement() throws ConfigurationException {
+        HierarchicalConfiguration config = Util.getCombinedConfiguration();
+        config.setExpressionEngine(new XPathExpressionEngine());
+
+        List < ? > endpoints = config.configurationsAt("hostEndPoints/hostEndPoint[@name = 'TheOtterrMainframe']");
+        assertTrue(endpoints.isEmpty());
+    }
+
+    /**
+     * Test reading the configuration directly.
+     * @throws ConfigurationException if reading fails
+     */
+    public void testReadConfigXPathDirect() throws ConfigurationException {
+        HierarchicalConfiguration config = Util.getCombinedConfiguration();
+        config.setExpressionEngine(new XPathExpressionEngine());
+
+        List < ? >  endpoints = config.configurationsAt("hostEndPoints/hostEndPoint[@name = 'TheOtherMainframe']");
+        assertTrue(!endpoints.isEmpty());
+        HierarchicalConfiguration endpoint = (HierarchicalConfiguration) endpoints.get(0);
+        assertEquals("TheOtherMainframe", endpoint.getString("@name"));
+        assertEquals("mainframe", endpoint.getString("hostIPAddress"));
+    }
+
 }
