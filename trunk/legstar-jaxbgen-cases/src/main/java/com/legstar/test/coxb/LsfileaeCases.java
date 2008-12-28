47bea919526f2a42401932fab1470fb9ccad6962
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2008-12-28 17:13:51 +0000

    Added a LSFILEAC test case

--- trunk/legstar-jaxbgen-cases/src/main/java/com/legstar/test/coxb/LsfileaeCases.java
+++ trunk/legstar-jaxbgen-cases/src/main/java/com/legstar/test/coxb/LsfileaeCases.java
@@ -47,6 +47,7 @@ public final class LsfileaeCases extends TestCase {
         assertEquals("00100.35", dfhcommarea.getComAmount().trim());
         assertEquals("A VOIR", dfhcommarea.getComComment().trim());
     }
+
     /**
      * @return a hexadecimal representation of host data.
      */
@@ -60,4 +61,51 @@ public final class LsfileaeCases extends TestCase {
         + "f0f0f1f0f04bf3f5"
         + "c140e5d6c9d9404040";
     }
+
+    /**
+     * @return a hexadecimal representation of host data.
+     */
+    public static String getHostBytesHexRequest100() { 
+
+        return
+        /*  0 0 0 1 0 0 */
+        "f0f0f0f1f0f0";
+    }
+
+    /**
+     * @return a hexadecimal representation of host data.
+     */
+    public static String getHostBytesHexReply100() { 
+
+        return
+        /*  0 0 0 1 0 0 */
+        "f0f0f0f1f0f0"
+        /*  S .  D .   B O R M A N       */
+        + "e24b40c44b40c2d6d9d4c1d54040404040404040"
+        /*  L A B A S   S T R E E T                 */
+        + "e2e4d9d9c5e86b40c5d5c7d3c1d5c44040404040"
+        /*  3 2 1 5 6 7 7 8 */
+        + "f3f2f1f5f6f7f7f8"
+        /*  2 6   1 1   8 1  */
+        + "f2f640f1f140f8f1"
+        /*  $ 0 1 0 0 . 1 1  */
+        + "5bf0f1f0f04bf1f1"
+        /*  * * * * * * * * *  */
+        + "5c5c5c5c5c5c5c5c5c";
+    }
+
+    /** 
+     * Check the values returned from LSFILAE after they were transformed to Java.
+     * @param dfhcommarea the java data object
+     */
+    public static void checkJavaObjectReply100(final Dfhcommarea dfhcommarea) {
+        assertEquals(100, dfhcommarea.getComNumber());
+        assertEquals("$0100.11", dfhcommarea.getComAmount());
+        assertEquals("*********", dfhcommarea.getComComment());
+        assertEquals("26 11 81", dfhcommarea.getComDate());
+        assertEquals("SURREY, ENGLAND", dfhcommarea.getComPersonal().getComAddress());
+        assertEquals("S. D. BORMAN", dfhcommarea.getComPersonal().getComName());
+        assertEquals("32156778", dfhcommarea.getComPersonal().getComPhone());
+    }
+
 }
