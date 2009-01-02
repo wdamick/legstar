4213070071062e3123fb94a589cf8e37567d6664
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2009-01-02 09:14:15 +0000

    Cleaning up (checkstyle) the test code.

--- trunk/legstar-cixsgen-model/src/test/java/com/legstar/cixs/gen/model/CixsGenModelTest.java
+++ trunk/legstar-cixsgen-model/src/test/java/com/legstar/cixs/gen/model/CixsGenModelTest.java
@@ -20,64 +20,82 @@ import org.xml.sax.InputSource;
 
 import junit.framework.TestCase;
 
+/**
+ * Test the CixsStructure class.
+ *
+ */
 public class CixsGenModelTest extends TestCase {
-	
-	public void testStructure() {
-		CixsStructure structure = new CixsStructure();
-		assertTrue(null == structure.getJaxbPropertyName());
-		structure.setJaxbFieldName("jaxbFieldName");
-		assertEquals("JaxbFieldName", structure.getJaxbPropertyName());
-		assertEquals("jaxbFieldName", structure.getJaxbFieldName());
-		structure.setJaxbFieldName(null);
-		assertTrue(null == structure.getJaxbFieldName());
-		structure.setJaxbPropertyName("JaxbPropertyName");
-		assertEquals("jaxbPropertyName", structure.getJaxbFieldName());
-		assertEquals("JaxbPropertyName", structure.getJaxbPropertyName());
-		structure.setJaxbPropertyName(null);
-		assertTrue(null == structure.getJaxbPropertyName());
-		structure.setJaxbType("JaxbType");
-		assertEquals("jaxb", structure.getJaxbFieldName());
-		assertEquals("Jaxb", structure.getJaxbPropertyName());
-	}
-	
-	public void testSerializeStructure() {
-		CixsStructure structure = new CixsStructure();
-		structure.setCicsContainer("cicsContainer");
-		structure.setCobolRootDataItemName("cobolRootDataItemName");
-		structure.setCoxbPackageName("coxbPackageName");
-		structure.setCoxbType("coxbType");
-		structure.setJaxbFieldName("jaxbFieldName");
-		structure.setJaxbPackageName("jaxbPackageName");
-		structure.setJaxbPropertyName("jaxbPropertyName");
-		structure.setJaxbType("jaxbType");
-		String result = structure.serialize("input");
-		System.out.println(result);
-		assertEquals(serializedStructure(), result);
-	}
-	
-	public void testLoadStructure() throws Exception {
-    	DocumentBuilderFactory docBuilderFactory =
-    		DocumentBuilderFactory.newInstance();
-    	DocumentBuilder docBuilder;
-		docBuilderFactory.setNamespaceAware(false);
-		docBuilder = docBuilderFactory.newDocumentBuilder();
-		Document doc = docBuilder.parse(new InputSource(new StringReader(serializedStructure())));
-		CixsStructure structure = new CixsStructure();
-		structure.load(doc.getFirstChild());
-		assertEquals("cicsContainer", structure.getCicsContainer());
-		assertEquals("cobolRootDataItemName", structure.getCobolRootDataItemName());
-		assertEquals("coxbPackageName", structure.getCoxbPackageName());
-		assertEquals("coxbType", structure.getCoxbType());
-		assertEquals("jaxbFieldName", structure.getJaxbFieldName());
-		assertEquals("jaxbPackageName", structure.getJaxbPackageName());
-		assertEquals("jaxbPropertyName", structure.getJaxbPropertyName());
-		assertEquals("jaxbType", structure.getJaxbType());
-	}
-	
-	private String serializedStructure() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("<input jaxbType=\"jaxbType\" jaxbPackageName=\"jaxbPackageName\" coxbType=\"coxbType\" coxbPackageName=\"coxbPackageName\" cobolRootDataItemName=\"cobolRootDataItemName\" cicsContainer=\"cicsContainer\" jaxbPropertyName=\"jaxbPropertyName\" jaxbFieldName=\"jaxbFieldName\"/>");
-		return sb.toString();
-	}
+    
+    /** A sample serialized structure.*/
+    public static final String SERIALIZED_STRUCTURE = "<input jaxbType=\"jaxbType\""
+        + " jaxbPackageName=\"jaxbPackageName\""
+        + " coxbType=\"coxbType\""
+        + " coxbPackageName=\"coxbPackageName\""
+        + " cobolRootDataItemName=\"cobolRootDataItemName\""
+        + " cicsContainer=\"cicsContainer\""
+        + " jaxbPropertyName=\"jaxbPropertyName\""
+        + " jaxbFieldName=\"jaxbFieldName\"/>";
+
+    /**
+     * Check members.
+     */
+    public void testStructure() {
+        CixsStructure structure = new CixsStructure();
+        assertTrue(null == structure.getJaxbPropertyName());
+        structure.setJaxbFieldName("jaxbFieldName");
+        assertEquals("JaxbFieldName", structure.getJaxbPropertyName());
+        assertEquals("jaxbFieldName", structure.getJaxbFieldName());
+        structure.setJaxbFieldName(null);
+        assertTrue(null == structure.getJaxbFieldName());
+        structure.setJaxbPropertyName("JaxbPropertyName");
+        assertEquals("jaxbPropertyName", structure.getJaxbFieldName());
+        assertEquals("JaxbPropertyName", structure.getJaxbPropertyName());
+        structure.setJaxbPropertyName(null);
+        assertTrue(null == structure.getJaxbPropertyName());
+        structure.setJaxbType("JaxbType");
+        assertEquals("jaxb", structure.getJaxbFieldName());
+        assertEquals("Jaxb", structure.getJaxbPropertyName());
+    }
+
+    /**
+     * Serialize a CixsStructure.
+     */
+    public void testSerializeStructure() {
+        CixsStructure structure = new CixsStructure();
+        structure.setCicsContainer("cicsContainer");
+        structure.setCobolRootDataItemName("cobolRootDataItemName");
+        structure.setCoxbPackageName("coxbPackageName");
+        structure.setCoxbType("coxbType");
+        structure.setJaxbFieldName("jaxbFieldName");
+        structure.setJaxbPackageName("jaxbPackageName");
+        structure.setJaxbPropertyName("jaxbPropertyName");
+        structure.setJaxbType("jaxbType");
+        String result = structure.serialize("input");
+        System.out.println(result);
+        assertEquals(SERIALIZED_STRUCTURE, result);
+    }
+
+    /**
+     * Deserialize a CixsStructure.
+     * @throws Exception if deserializing fails
+     */
+    public void testLoadStructure() throws Exception {
+        DocumentBuilderFactory docBuilderFactory =
+            DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder;
+        docBuilderFactory.setNamespaceAware(false);
+        docBuilder = docBuilderFactory.newDocumentBuilder();
+        Document doc = docBuilder.parse(new InputSource(new StringReader(SERIALIZED_STRUCTURE)));
+        CixsStructure structure = new CixsStructure();
+        structure.load(doc.getFirstChild());
+        assertEquals("cicsContainer", structure.getCicsContainer());
+        assertEquals("cobolRootDataItemName", structure.getCobolRootDataItemName());
+        assertEquals("coxbPackageName", structure.getCoxbPackageName());
+        assertEquals("coxbType", structure.getCoxbType());
+        assertEquals("jaxbFieldName", structure.getJaxbFieldName());
+        assertEquals("jaxbPackageName", structure.getJaxbPackageName());
+        assertEquals("jaxbPropertyName", structure.getJaxbPropertyName());
+        assertEquals("jaxbType", structure.getJaxbType());
+    }
 
 }
