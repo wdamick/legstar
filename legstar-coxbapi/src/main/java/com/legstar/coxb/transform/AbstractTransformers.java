8578dc82d1c024d629c2ce8afb2c2ba98e153a04
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2009-01-05 13:53:44 +0000

    Renamed AbstractTransformerProvider to AbstractTransformers which is shorter and also remove methods implementations which had no added value.

---
+++ legstar-coxbapi/src/main/java/com/legstar/coxb/transform/AbstractTransformers.java
@@ -0,0 +1,70 @@
+package com.legstar.coxb.transform;
+
+
+/**
+ * A generic class that provides transformer capabilities for a given structure.
+ * <p/>
+ * A structure maps to a java class and a COBOL structure.
+ * This class does not implement the transformers, it acts as a container.
+ * <p/>
+ * Classes derived from this one will typically implement a constructor that
+ * creates the directional transformers, java to host and host to java.
+ *
+ */
+public abstract class AbstractTransformers {
+
+    /** Transformer that turns a java data object into host data. */
+    private IJavaToHostTransformer mJavaToHost;
+
+    /** Transformer that turns host data into a java data object. */
+    private IHostToJavaTransformer mHostToJava;
+    
+    /**
+     * No arg constructor. Caller is responsible for setting the internal transformers.
+     */
+    public AbstractTransformers() {
+        
+    }
+    
+    /**
+     * Creates a provider with its directional transformers.
+     * @param javaToHost java to host transformer
+     * @param hostToJava host to java transformer
+     */
+    public AbstractTransformers(
+            final IJavaToHostTransformer javaToHost,
+            final IHostToJavaTransformer hostToJava) {
+        mJavaToHost = javaToHost;
+        mHostToJava = hostToJava;
+    }
+    /**
+     * @return the transformer that turns a java data object into host data
+     */
+    public IJavaToHostTransformer getJavaToHost() {
+        return mJavaToHost;
+    }
+
+    /**
+     * @param javaToHost the the transformer that turns a java data object into host data to set
+     */
+    public void setJavaToHost(
+            final IJavaToHostTransformer javaToHost) {
+        mJavaToHost = javaToHost;
+    }
+
+    /**
+     * @return the transformer that turns host data into a java data object
+     */
+    public IHostToJavaTransformer getHostToJava() {
+        return mHostToJava;
+    }
+
+     /**
+     * @param hostToJava the transformer that turns host data into a java data object to set
+     */
+    public void setHostToJava(
+            final IHostToJavaTransformer hostToJava) {
+        mHostToJava = hostToJava;
+    }
+
+}
