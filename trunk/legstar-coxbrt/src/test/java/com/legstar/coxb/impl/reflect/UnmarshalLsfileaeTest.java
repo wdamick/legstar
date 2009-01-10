cd101954fa174093fb08a0893218c95c5f6d2b96
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2009-01-10 15:20:11 +0000

    Added an implementation of generic transformers using reflection. This allows upstream modules to easily swith from generated transformers to dynamic ones using reflection.

--- trunk/legstar-coxbrt/src/test/java/com/legstar/coxb/impl/reflect/UnmarshalLsfileaeTest.java
+++ trunk/legstar-coxbrt/src/test/java/com/legstar/coxb/impl/reflect/UnmarshalLsfileaeTest.java
@@ -14,49 +14,55 @@ import com.legstar.coxb.CobolContext;
 import com.legstar.coxb.convert.simple.CobolSimpleConverters;
 import com.legstar.coxb.impl.reflect.CComplexReflectBinding;
 import com.legstar.coxb.impl.visitor.CobolUnmarshalVisitor;
+import com.legstar.coxb.transform.HostTransformException;
 import com.legstar.coxb.host.HostData;
 import com.legstar.coxb.host.HostException;
 
 import junit.framework.TestCase;
+
+import com.legstar.test.coxb.LsfileaeCases;
 import com.legstar.test.coxb.lsfileae.ObjectFactory;
 import com.legstar.test.coxb.lsfileae.Dfhcommarea;
 
+/**
+ * Test LSFILEAE.
+ *
+ */
 public class UnmarshalLsfileaeTest extends TestCase {
-	/**
-	 * Unmarshal Lsfileae.
-	 * @throws HostException if anything goes wrong
-	 * @throws ClassNotFoundException 
-	 */
-	public final void testLsfileae() throws HostException, ClassNotFoundException {
-		// Create a cobol context 
-		CobolContext cobolContext = new CobolContext();
-		// Select a conversion strategy 
-		CobolSimpleConverters cc = new CobolSimpleConverters(cobolContext);
-		//		            <----------><--------------------------------------><--------------------------------------><--------------><--------------><--------------><---------------->
-		//		            1 2 3 4 5 6 1 2 3 4 5 6 7 8 9 10111213141516171819201 2 3 4 5 6 7 8 9 10111213141516171819201 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8 9
-		//		            0 0 0 1 0 0 T O T O                                 L A B A S   S T R E E T                 8 8 9 9 3 3 1 4 1 0 0 4 5 8 0 0 1 0 0 . 3 5 A   V O I R
-		String hexString = "f0f0f0f1f0f0e3d6e3d640404040404040404040404040404040d3c1c2c1e240e2e3d9c5c5e34040404040404040f8f8f9f9f3f3f1f4f1f0f0f4f5f84040f0f0f1f0f04bf3f5c140e5d6c9d9404040";
-		byte[] hostBytes = HostData.toByteArray(hexString);
 
-		// Create a concrete visitor
-		CobolUnmarshalVisitor uv = new CobolUnmarshalVisitor(hostBytes, 0, cc);
-		
-		// Create an instance of the JAXB object factory
-		ObjectFactory objectFactory = new ObjectFactory();
+    /**
+     * Unmarshal Lsfileae.
+     * @throws HostException if anything goes wrong
+     * @throws ClassNotFoundException 
+     */
+    public final void testLsfileae() throws HostException, ClassNotFoundException {
+        CobolContext cobolContext = new CobolContext();
+        CobolSimpleConverters cc = new CobolSimpleConverters(cobolContext);
+        byte[] hostBytes = HostData.toByteArray(LsfileaeCases.getHostBytesHex());
+        CobolUnmarshalVisitor uv = new CobolUnmarshalVisitor(hostBytes, 0, cc);
+        ObjectFactory objectFactory = new ObjectFactory();
+        CComplexReflectBinding ccem = new CComplexReflectBinding(objectFactory,
+                Dfhcommarea.class);
+        ccem.accept(uv);
+        Dfhcommarea dfhcommarea = (Dfhcommarea) ccem.getObjectValue(Dfhcommarea.class);
+        LsfileaeCases.checkJavaObject(dfhcommarea);
+    }
 
-		// Traverse the object structure, visiting each node with the visitor
-		CComplexReflectBinding ccem = new CComplexReflectBinding(objectFactory,
-				Dfhcommarea.class);
-		ccem.accept(uv);
-		Dfhcommarea dfhcommarea = (Dfhcommarea) ccem.getObjectValue(Dfhcommarea.class);
-		
-		assertEquals(100, dfhcommarea.getComNumber());
-		assertEquals("TOTO", dfhcommarea.getComPersonal().getComName().trim());
-		assertEquals("LABAS STREET", dfhcommarea.getComPersonal().getComAddress().trim());
-		assertEquals("88993314", dfhcommarea.getComPersonal().getComPhone().trim());
-		assertEquals("100458", dfhcommarea.getComDate().trim());
-		assertEquals("00100.35", dfhcommarea.getComAmount().trim());
-		assertEquals("A VOIR", dfhcommarea.getComComment().trim());
-	}
-
+    /**
+     * Test Transforming.
+     */
+    public void testTransformLsfileae() {
+        try {
+            ReflectHostToJavaTransformer transformer =
+                new ReflectHostToJavaTransformer(
+                        "com.legstar.test.coxb.lsfileae", "Dfhcommarea");
+            Dfhcommarea dfhcommarea = transformer.transform(
+                    HostData.toByteArray(LsfileaeCases.getHostBytesHex()));
+            LsfileaeCases.checkJavaObject(dfhcommarea);
+        } catch (ReflectBindingException e) {
+            fail(e.getMessage());
+        } catch (HostTransformException e) {
+            fail(e.getMessage());
+        }
+    }
 }
