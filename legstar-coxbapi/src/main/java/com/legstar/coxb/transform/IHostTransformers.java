f6e81dd03350e3ffbda2b96a7675b57a3b3c26f6
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2009-01-10 15:30:32 +0000

    Added an interface for transformers. This significantly reduces the nedd to generate code as the AbstractTransformers can implement most of the logic.

---
+++ legstar-coxbapi/src/main/java/com/legstar/coxb/transform/IHostTransformers.java
@@ -0,0 +1,67 @@
+package com.legstar.coxb.transform;
+
+
+/**
+ * Classes implementing this interface group transformers for a given
+ * complex type.
+ *
+ */
+public interface IHostTransformers {
+
+    /**
+     * Transforms java data object to host data with a specific host character set.
+     * @param valueObject a java value object
+     * @param hostCharset the host character set
+     * @return a byte array with host data
+     * @throws HostTransformException if transformation fails
+     */
+    byte[] toHost(
+            final Object valueObject, final String hostCharset) throws HostTransformException;
+
+    /**
+     * Transforms java data object to host data.
+     * @param valueObject a java value object
+     * @return a byte array with host data
+     * @throws HostTransformException if transformation fails
+     */
+    byte[] toHost(final Object valueObject) throws HostTransformException;
+
+    /**
+     * Transforms host data to java data object with a specific host character set.
+     * @param hostData a byte array containing host data
+     * @param hostCharset the host character set
+     * @return a Java value object
+     * @throws HostTransformException if transformation fails
+     */
+    Object toJava(final byte[] hostData, final String hostCharset)
+            throws HostTransformException;
+
+    /**
+     * Transforms host data to java data object.
+     * @param hostData a byte array containing host data
+     * @return a Java value object
+     * @throws HostTransformException if transformation fails
+     */
+    Object toJava(final byte[] hostData) throws HostTransformException;
+
+    /**
+     * @return the transformer that turns a java data object into host data
+     */
+    IJavaToHostTransformer getJavaToHost();
+
+    /**
+     * @param javaToHost the the transformer that turns a java data object into host data to set
+     */
+    void setJavaToHost(final IJavaToHostTransformer javaToHost);
+
+    /**
+     * @return the transformer that turns host data into a java data object
+     */
+    IHostToJavaTransformer getHostToJava();
+
+    /**
+     * @param hostToJava the transformer that turns host data into a java data object to set
+     */
+    void setHostToJava(final IHostToJavaTransformer hostToJava);
+
+}
