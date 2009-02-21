e7a958178bb00510dba11d2b10508df1bf72e984
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2009-02-21 16:38:33 +0000

    Adding a JMeter ready test for transformers (reflection binding).

--- trunk/legstar-coxbrt/src/test/java/com/legstar/coxb/transform/AbstractHostToJavaTransformerTest.java
+++ trunk/legstar-coxbrt/src/test/java/com/legstar/coxb/transform/AbstractHostToJavaTransformerTest.java
@@ -13,64 +13,29 @@ package com.legstar.coxb.transform;
 import com.legstar.coxb.host.HostData;
 import com.legstar.test.coxb.lsfileae.Dfhcommarea;
 
-import junit.framework.TestCase;
 
 /**
  * Test AbstractHostToJavaTransformer.
  */
-public class AbstractHostToJavaTransformerTest extends TestCase {
+public class AbstractHostToJavaTransformerTest extends AbstractTestTransformers {
     
-    /** A raw zos serialization. */
-    private static final String RAW_LSFILEAE_DATA =
-       /* 0 0 0 1 0 0 T O T O                                 L A B A S ç S T R E E T                */
-        "f0f0f0f1f0f0e3d6e3d640404040404040404040404040404040d3c1c2c1e2e0e2e3d9c5c5e34040404040404040"
-       /* 8 8 9 9 3 3 1 4 1 0 0 4 5 8     0 0 1 0 0 . 3 5 A   V O I R      */
-     +  "f8f8f9f9f3f3f1f4f1f0f0f4f5f84040f0f0f1f0f04bf3f5c140e5d6c9d9404040";
-
     /**
      * Test transforming a byte array to java object (using reflection binding).
      * @throws HostTransformException if transform fails
      */
     public void testTransform() throws HostTransformException {
         HostToJavaLsfileaeTransformer hostToJavaTransformer = new HostToJavaLsfileaeTransformer();
-        Object javaValue = hostToJavaTransformer.transform(HostData.toByteArray(RAW_LSFILEAE_DATA));
+        Object javaValue = hostToJavaTransformer.transform(
+                HostData.toByteArray(RAW_LSFILEAE_DATA_IBM01147));
         assertTrue(null != javaValue);
         assertTrue(javaValue instanceof Dfhcommarea);
         checkLsfileaeIBM01140((Dfhcommarea) javaValue);
         
-        javaValue = hostToJavaTransformer.transform(HostData.toByteArray(RAW_LSFILEAE_DATA), "IBM01147");
+        javaValue = hostToJavaTransformer.transform(
+                HostData.toByteArray(RAW_LSFILEAE_DATA_IBM01147), STRING_FRENCH_CHARSET);
         assertTrue(null != javaValue);
         assertTrue(javaValue instanceof Dfhcommarea);
         checkLsfileaeIBM01147((Dfhcommarea) javaValue);
     }
     
-    /**
-     * Check all member variables from value object with US character set.
-     * @param dfhcommarea the value object
-     */
-    private void checkLsfileaeIBM01140(final Dfhcommarea dfhcommarea) {
-        assertEquals("00100.35", dfhcommarea.getComAmount());
-        assertEquals("A VOIR", dfhcommarea.getComComment());
-        assertEquals("100458", dfhcommarea.getComDate());
-        assertEquals(100, dfhcommarea.getComNumber());
-        assertEquals("LABAS\\STREET", dfhcommarea.getComPersonal().getComAddress());
-        assertEquals("TOTO", dfhcommarea.getComPersonal().getComName());
-        assertEquals("88993314", dfhcommarea.getComPersonal().getComPhone());
-    }
-    
-    /**
-     * Check all member variables from value object with French character set.
-     * @param dfhcommarea the value object
-     */
-    private void checkLsfileaeIBM01147(final Dfhcommarea dfhcommarea) {
-        assertEquals("00100.35", dfhcommarea.getComAmount());
-        assertEquals("A VOIR", dfhcommarea.getComComment());
-        assertEquals("100458", dfhcommarea.getComDate());
-        assertEquals(100, dfhcommarea.getComNumber());
-        assertEquals("LABASçSTREET", dfhcommarea.getComPersonal().getComAddress());
-        assertEquals("TOTO", dfhcommarea.getComPersonal().getComName());
-        assertEquals("88993314", dfhcommarea.getComPersonal().getComPhone());
-    }
-
-
 }
---
+++ trunk/legstar-coxbrt/src/test/java/com/legstar/coxb/transform/LsfileaeTransformers.java
@@ -0,0 +1,41 @@
+package com.legstar.coxb.transform;
+
+/**
+ * A transformers class where individual transformers use reflection
+ * on JAXB classes to perform the actual binding.
+ *
+ */
+public class LsfileaeTransformers extends AbstractTransformers {
+
+    
+    /**
+     * Creates a transformer for each direction at construction time.
+     */
+    public LsfileaeTransformers() {
+        super(new JavaToHostLsfileaeTransformer(),
+                new HostToJavaLsfileaeTransformer());
+    }
+
+    /** {@inheritDoc}*/
+    public byte[] toHost(final Object valueObject, final String hostCharset)
+            throws HostTransformException {
+        return getJavaToHost().transform(valueObject, hostCharset);
+    }
+
+    /** {@inheritDoc}*/
+    public byte[] toHost(final Object valueObject) throws HostTransformException {
+        return getJavaToHost().transform(valueObject);
+    }
+
+    /** {@inheritDoc}*/
+    public Object toJava(final byte[] hostData, final String hostCharset)
+            throws HostTransformException {
+        return getHostToJava().transform(hostData, hostCharset);
+    }
+
+    /** {@inheritDoc}*/
+    public Object toJava(final byte[] hostData) throws HostTransformException {
+        return getHostToJava().transform(hostData);
+    }
+
+}
