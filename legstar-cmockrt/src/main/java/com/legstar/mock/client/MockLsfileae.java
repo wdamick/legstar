6c36da4c3a38c63e7501b94abc22a4f8434b6a82
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2009-03-03 12:09:40 +0000

    Added a mock connection project to allow adapters to be tested without a mainframe connection. It is not extensible at this stage (cases are predefined).

---
+++ legstar-cmockrt/src/main/java/com/legstar/mock/client/MockLsfileae.java
@@ -0,0 +1,70 @@
+/*******************************************************************************
+ * Copyright (c) 2009 LegSem.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v2.1
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     LegSem - initial API and implementation
+ ******************************************************************************/
+package com.legstar.mock.client;
+
+import java.io.IOException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.legstar.coxb.transform.HostTransformException;
+import com.legstar.messaging.CommareaPart;
+import com.legstar.messaging.HeaderPartException;
+import com.legstar.messaging.LegStarMessage;
+import com.legstar.messaging.RequestException;
+import com.legstar.test.coxb.lsfileae.Dfhcommarea;
+import com.legstar.test.coxb.lsfileae.bind.DfhcommareaTransformers;
+
+/**
+ * Mocks the behavior of the LSFILEAE program.
+ *
+ */
+public final class MockLsfileae {
+
+    /** Logger. */
+    private static final Log LOG = LogFactory.getLog(MockLsfileae.class);
+    
+    /** Utility class.*/
+    private MockLsfileae() {
+        
+    }
+
+    /**
+     * Create a response to LSFILEAE execution request.
+     * @param requestMessage the request message
+     * @return formatted response
+     * @throws RequestException if response cannot be built
+     */
+    public static  LegStarMessage getResponse(
+            final LegStarMessage requestMessage) throws RequestException {
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Building response for program LSFILEAE");
+        }
+        try {
+            DfhcommareaTransformers transformers = new DfhcommareaTransformers();
+            Dfhcommarea dfhcommareaRequest =  transformers.toJava(
+                    requestMessage.getDataParts().get(0).getContent());
+            Dfhcommarea dfhcommareaResponse = new MockFILEA().getCustomer(
+                    dfhcommareaRequest.getComNumber());
+            LegStarMessage replyMessage = new LegStarMessage();
+            replyMessage.addDataPart(new CommareaPart(
+                    transformers.toHost(dfhcommareaResponse)));
+            return replyMessage;
+        } catch (HeaderPartException e) {
+            throw new RequestException(e);
+        } catch (HostTransformException e) {
+            throw new RequestException(e);
+        } catch (IOException e) {
+            throw new RequestException(e);
+        }
+    }
+    
+}
