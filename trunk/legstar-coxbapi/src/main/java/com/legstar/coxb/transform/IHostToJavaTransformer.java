629291e0cf685788ec6d63cc357c3cf2f5eff2fc
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2009-03-10 14:45:30 +0000

    Issue 42. There is an additional method for Host To Java transformers that takes an offset from the input host byte array.

--- trunk/legstar-coxbapi/src/main/java/com/legstar/coxb/transform/IHostToJavaTransformer.java
+++ trunk/legstar-coxbapi/src/main/java/com/legstar/coxb/transform/IHostToJavaTransformer.java
@@ -28,6 +28,17 @@ public interface IHostToJavaTransformer extends IHostTransformer {
     < T > T  transform(final byte[] hostData, final String hostCharset) throws HostTransformException;
 
     /**
+     * Transforms host data to java with a specific host character set.
+     * @param <T> the bound object type
+     * @param hostData a byte array containing host data
+     * @param offset index of first byte to process in hostData 
+     * @param hostCharset the host character set
+     * @return a Java value object
+     * @throws HostTransformException if transformation fails
+     */
+    < T > T  transform(final byte[] hostData, final int offset, final String hostCharset) throws HostTransformException;
+
+    /**
      * Transforms host data to java.
      * @param <T> the bound object type
      * @param hostData a byte array containing host data
@@ -36,4 +47,13 @@ public interface IHostToJavaTransformer extends IHostTransformer {
      */
     < T > T transform(final byte[] hostData) throws HostTransformException;
 
+    /**
+     * Transforms host data to java.
+     * @param <T> the bound object type
+     * @param hostData a byte array containing host data
+     * @param offset index of first byte to process in hostData 
+     * @return a Java value object
+     * @throws HostTransformException if transformation fails
+     */
+    < T > T transform(final byte[] hostData, final int offset) throws HostTransformException;
 }
