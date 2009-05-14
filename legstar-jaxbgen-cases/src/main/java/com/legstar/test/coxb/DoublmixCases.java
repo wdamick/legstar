d94a710d90c25e841009ce62b32cb90ff3a13d10
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2009-05-14 10:12:02 +0000

    Update test cases due to fix for Issue 48 on double conversions.

--- legstar-jaxbgen-cases/src/main/java/com/legstar/test/coxb/DoublmixCases.java
+++ legstar-jaxbgen-cases/src/main/java/com/legstar/test/coxb/DoublmixCases.java
@@ -49,16 +49,16 @@ public final class DoublmixCases extends TestCase {
         assertEquals(0d, dfhcommarea.getCDouble0());
         assertEquals(1d, dfhcommarea.getCDouble1());
         assertEquals(1234d, dfhcommarea.getCDouble1234());
-        assertEquals(345006.56779999984d, dfhcommarea.getCDouble345006P5678());
-        assertEquals(3.4028234699999995E+38, dfhcommarea.getCDouble3P40282347Ep38());
-        assertEquals(7.982006699999985E-14, dfhcommarea.getCDouble798P20067Em16());
+        assertEquals(345006.56779999996d, dfhcommarea.getCDouble345006P5678());
+        assertEquals(3.40282347E+38, dfhcommarea.getCDouble3P40282347Ep38());
+        assertEquals(7.982006699999995E-14, dfhcommarea.getCDouble798P20067Em16());
     }
     /**
      * @return a hexadecimal representation of host data.
      */
     public static String getHostBytesHex() { 
 
-        return "434d2000000000000000000000000000411000000000000045543ae915b573e0361677a4590fab6060ffffff048ff9e0";
+        return "434d2000000000000000000000000000411000000000000045543ae915b573e8361677a4590fab6860ffffff048ff9e8";
     }
 
     /**
