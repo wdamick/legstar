cbd3a0723b96bcf4a72a1a1845a94826ed190c5b
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2009-07-08 07:50:35 +0000

    Added a short version of the constructor.

--- trunk/legstar-invoker/src/main/java/com/legstar/host/invoke/model/HostContainer.java
+++ trunk/legstar-invoker/src/main/java/com/legstar/host/invoke/model/HostContainer.java
@@ -23,6 +23,23 @@ public class HostContainer {
     private int _length;
 
     /**
+     * Bean constructor.
+     */
+    public HostContainer() {
+        
+    }
+
+    /**
+     * Shorthand constructor.
+     * @param name container name
+     * @param length container maximum size
+     */
+    public HostContainer(final String name, final int length) {
+        _name = name;
+        _length = length;
+    }
+
+    /**
      * @return the container name.
      */
     public final String getName() {
