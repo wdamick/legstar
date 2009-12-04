10388b6bf7660644468c5625ea6fd6d03b8d717e
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2009-12-04 18:16:29 +0000

    Replaced legstar-schemagen with legstar-cob2xsd in the Eclipse plugins. There should be no more references to legstar-schemagen throughout the product now.

--- trunk/legstar-schemagen-plugin/src/main/java/com/legstar/eclipse/plugin/schemagen/preferences/PreferenceConstants.java
+++ trunk/legstar-schemagen-plugin/src/main/java/com/legstar/eclipse/plugin/schemagen/preferences/PreferenceConstants.java
@@ -15,14 +15,72 @@ package com.legstar.eclipse.plugin.schemagen.preferences;
  */
 public final class PreferenceConstants {
 
+ 
+    /* -------------------------------------------------------------------
+     * XML Schema related default options
+     * */
     /** A prefix to be used for new XML namespaces. */
     public static final String XSD_NAMESPACE_PREFIX =
         "com.legstar.eclipse.plugin.schemagen.xsdNamespacePrefix";
 
+    /** XML schema default encoding. */
+    public static final String DEFAULT_XSD_ENCODING =
+        "com.legstar.eclipse.plugin.schemagen.defaultXsdEncoding";
+
+    /** XML schema default mapping of 88 conditions to facets. */
+    public static final String DEFAULT_XSD_MAP_CONDITIONS_TO_FACETS =
+        "com.legstar.eclipse.plugin.schemagen.defaultMapConditionsToFacets";
+
+    /** XML schema default customization XSLT file name. */
+    public static final String DEFAULT_XSD_CUSTOM_XSLT_FILE_NAME =
+        "com.legstar.eclipse.plugin.schemagen.defaultCustomXsltFileName";
+
+    /** XML schema default prepending parent name if name conflict. */
+    public static final String DEFAULT_XSD_NAME_CONFLICT_PREPEND_PARENT_NAME =
+        "com.legstar.eclipse.plugin.schemagen.defaultNameConflictPrependParentName";
+
+    /** XML schema default element name starting with uppercase. */
+    public static final String DEFAULT_XSD_ELEMENT_NAMES_START_WITH_UPPERCASE =
+        "com.legstar.eclipse.plugin.schemagen.defaultElementNamesStartWithUppercase";
+    
+    /* -------------------------------------------------------------------
+     * LegStar annotations default options
+     * */
+    /** Annotations default presence. */
+    public static final String DEFAULT_ADD_LEGSTAR_ANNOTATIONS =
+        "com.legstar.eclipse.plugin.schemagen.defaultAddLegStarAnnotations";
+    
     /** Prefix to be used for new jaxb package names. */
     public static final String JAXB_PACKAGE_NAME_PREFIX =
         "com.legstar.eclipse.plugin.schemagen.jaxbPackageNamePrefix";
 
+    /** Annotations default suffix for type names. */
+    public static final String DEFAULT_JAXB_TYPE_CLASSES_SUFFIX =
+        "com.legstar.eclipse.plugin.schemagen.defaultJaxbTypeClassesSuffix";
+    
+   
+    /* -------------------------------------------------------------------
+     * COBOL compiler related options
+     * */
+    /** Currency symbol default. */
+    public static final String DEFAULT_CURRENCY_SYMBOL =
+        "com.legstar.eclipse.plugin.schemagen.defaultCurrencySymbol";
+
+    /** Decimal point is comma default. */
+    public static final String DEFAULT_DECIMAL_POINT_IS_COMMA =
+        "com.legstar.eclipse.plugin.schemagen.defaultDecimalPointIsComma";
+
+    /** National symbol DBCS default. */
+    public static final String DEFAULT_NSYMBOL_DBCS =
+        "com.legstar.eclipse.plugin.schemagen.defaultNSymbolDbcs";
+
+    /** Quote is quote default. */
+    public static final String DEFAULT_QUOTE_IS_QUOTE =
+        "com.legstar.eclipse.plugin.schemagen.defaultQuoteIsQuote";
+
+
+    
+    
     /** Last eclipse container used. */
     public static final String LAST_TARGET_CONTAINER =
         "com.legstar.eclipse.plugin.schemagen.last.targetcontainer";
