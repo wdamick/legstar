553f45ef7cac103f0bca8fca2ef9349db56ad399
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2010-07-24 07:40:48 +0000

    Removed IAM incremental builder from te Eclipse build command sequence. The problem is that IAM comes with Maven 2.1 while some our our POM syntax requires Maven 2.2.

--- legstar-common-plugin/src/main/java/com/legstar/eclipse/plugin/common/preferences/PreferenceConstants.java
+++ legstar-common-plugin/src/main/java/com/legstar/eclipse/plugin/common/preferences/PreferenceConstants.java
@@ -13,7 +13,7 @@ package com.legstar.eclipse.plugin.common.preferences;
 /**
  * Constant definitions for plug-in preferences.
  */
-public class PreferenceConstants {
+public final class PreferenceConstants {
 
     /** Folder where ant scripts should be generated. */
     public static final String ANT_SCRIPTS_FOLDER =
