efc72d4d303694ae4fffae4f58d42af549d9dc72
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2010-10-09 10:36:54 +0000

    MS complains about the repetitive attempts.

--- legstar-c2wsrt/src/test/java/com/legstar/proxy/invoke/jaxws/WebServiceInvokerTest.java
+++ legstar-c2wsrt/src/test/java/com/legstar/proxy/invoke/jaxws/WebServiceInvokerTest.java
@@ -10,7 +10,6 @@
  ******************************************************************************/
 package com.legstar.proxy.invoke.jaxws;
 
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,7 +24,7 @@ import junit.framework.TestCase;
 
 /**
  * Test WebServiceInvoker.
- *
+ * 
  */
 public class WebServiceInvokerTest extends TestCase {
 
@@ -37,78 +36,93 @@ public class WebServiceInvokerTest extends TestCase {
         try {
             new WebServiceInvoker(config);
         } catch (WebServiceInvokerException e) {
-            assertEquals("You must specify a wsdl URL using the wsdlUrl attribute", e.getMessage());
+            assertEquals(
+                    "You must specify a wsdl URL using the wsdlUrl attribute",
+                    e.getMessage());
         }
         config.put(WebServiceInvoker.WSDL_URL_PROPERTY, "http://org.url");
         try {
             new WebServiceInvoker(config);
         } catch (WebServiceInvokerException e) {
-            assertEquals("You must specify a wsdl target namespace using the wsdlTargetNamespace attribute",
+            assertEquals(
+                    "You must specify a wsdl target namespace using the wsdlTargetNamespace attribute",
                     e.getMessage());
         }
-        config.put(WebServiceInvoker.WSDL_TARGET_NAMESPACE_PROPERTY, "http://org.url");
+        config.put(WebServiceInvoker.WSDL_TARGET_NAMESPACE_PROPERTY,
+                "http://org.url");
         try {
             new WebServiceInvoker(config);
         } catch (WebServiceInvokerException e) {
-            assertEquals("You must specify a service name using the wsdlServiceName attribute",
+            assertEquals(
+                    "You must specify a service name using the wsdlServiceName attribute",
                     e.getMessage());
         }
         config.put(WebServiceInvoker.WSDL_SERVICE_NAME_PROPERTY, "serviceName");
         try {
             new WebServiceInvoker(config);
         } catch (WebServiceInvokerException e) {
-            assertEquals("You must specify a port name using the wsdlPortName attribute",
+            assertEquals(
+                    "You must specify a port name using the wsdlPortName attribute",
                     e.getMessage());
         }
         config.put(WebServiceInvoker.WSDL_PORT_NAME_PROPERTY, "portName");
         try {
             new WebServiceInvoker(config);
         } catch (WebServiceInvokerException e) {
-            assertEquals("You must specify a jaxb request type using the requestJaxbType attribute",
+            assertEquals(
+                    "You must specify a jaxb request type using the requestJaxbType attribute",
                     e.getMessage());
         }
         config.put(WebServiceInvoker.REQUEST_JAXB_TYPE_PROPERTY, "jaxbType");
         try {
             new WebServiceInvoker(config);
         } catch (WebServiceInvokerException e) {
-            assertEquals("You must specify a jaxb request package name using the requestJaxbPackageName attribute",
+            assertEquals(
+                    "You must specify a jaxb request package name using the requestJaxbPackageName attribute",
                     e.getMessage());
         }
-        config.put(WebServiceInvoker.REQUEST_JAXB_PACKAGE_NAME_PROPERTY, "jaxbPackage");
+        config.put(WebServiceInvoker.REQUEST_JAXB_PACKAGE_NAME_PROPERTY,
+                "jaxbPackage");
         try {
             new WebServiceInvoker(config);
         } catch (WebServiceInvokerException e) {
-            assertEquals("You must specify a jaxb response type using the responseJaxbType attribute",
+            assertEquals(
+                    "You must specify a jaxb response type using the responseJaxbType attribute",
                     e.getMessage());
         }
         config.put(WebServiceInvoker.RESPONSE_JAXB_TYPE_PROPERTY, "jaxbType");
         try {
             new WebServiceInvoker(config);
         } catch (WebServiceInvokerException e) {
-            assertEquals("You must specify a jaxb response package name using the responseJaxbPackageName attribute",
+            assertEquals(
+                    "You must specify a jaxb response package name using the responseJaxbPackageName attribute",
                     e.getMessage());
         }
-        config.put(WebServiceInvoker.RESPONSE_JAXB_PACKAGE_NAME_PROPERTY, "jaxbPackage");
+        config.put(WebServiceInvoker.RESPONSE_JAXB_PACKAGE_NAME_PROPERTY,
+                "jaxbPackage");
         try {
             new WebServiceInvoker(config);
         } catch (WebServiceInvokerException e) {
-            assertEquals("com.legstar.util.JAXBAnnotationException: java.lang.ClassNotFoundException:"
-                    + " jaxbPackage.jaxbType",
+            assertEquals(
+                    "com.legstar.util.JAXBAnnotationException: java.lang.ClassNotFoundException:"
+                            + " jaxbPackage.jaxbType",
                     e.getMessage());
         }
         config.put(WebServiceInvoker.REQUEST_JAXB_TYPE_PROPERTY, "Search");
         config.put(WebServiceInvoker.REQUEST_JAXB_PACKAGE_NAME_PROPERTY,
-        "com.legstar.test.coxb.MSNSearch");
+                "com.legstar.test.coxb.MSNSearch");
         try {
             new WebServiceInvoker(config);
         } catch (WebServiceInvokerException e) {
-            assertEquals("com.legstar.util.JAXBAnnotationException: java.lang.ClassNotFoundException:"
-                    + " jaxbPackage.jaxbType",
+            assertEquals(
+                    "com.legstar.util.JAXBAnnotationException: java.lang.ClassNotFoundException:"
+                            + " jaxbPackage.jaxbType",
                     e.getMessage());
         }
-        config.put(WebServiceInvoker.RESPONSE_JAXB_TYPE_PROPERTY, "SearchResponse");
+        config.put(WebServiceInvoker.RESPONSE_JAXB_TYPE_PROPERTY,
+                "SearchResponse");
         config.put(WebServiceInvoker.RESPONSE_JAXB_PACKAGE_NAME_PROPERTY,
-        "com.legstar.test.coxb.MSNSearch");
+                "com.legstar.test.coxb.MSNSearch");
         try {
             new WebServiceInvoker(config);
         } catch (WebServiceInvokerException e) {
@@ -117,21 +131,24 @@ public class WebServiceInvokerTest extends TestCase {
 
     }
 
-
     /**
      * Test the dispatch method.
+     * 
      * @throws Exception if test fails
      */
     public void testInvokeMSNSearch() throws Exception {
 
         /* Setup a configuration and instantiate action. */
-        WebServiceInvoker invoker = new WebServiceInvoker(MSNSearchJaxwsCases.getReflectConfig());
+        WebServiceInvoker invoker = new WebServiceInvoker(MSNSearchJaxwsCases
+                .getReflectConfig());
 
         /* Invoke the web service several times. */
         try {
             for (int i = 0; i < 5; i++) {
-                SearchResponse searchResponse = invoker.invoke(getName(), getSearchRequest());
-                assertTrue(searchResponse.getResponse().getResponses().getSourceResponse().size() > 0);
+                SearchResponse searchResponse = invoker.invoke(getName(),
+                        getSearchRequest());
+                assertTrue(searchResponse.getResponse().getResponses()
+                        .getSourceResponse().size() > 0);
             }
 
         } catch (Exception e) {
@@ -146,16 +163,18 @@ public class WebServiceInvokerTest extends TestCase {
     public static Search getSearchRequest() {
         /* Create a JAXB request object. */
         com.legstar.test.coxb.MSNSearch.ObjectFactory jaxbFactory =
-            new com.legstar.test.coxb.MSNSearch.ObjectFactory();
+                new com.legstar.test.coxb.MSNSearch.ObjectFactory();
 
         SourceRequestType sourceRequestType = new SourceRequestType();
         sourceRequestType.setSource(SourceTypeType.WEB);
         sourceRequestType.setCount(10);
 
-        ArrayOfSourceRequestRequestsType sr = jaxbFactory.createArrayOfSourceRequestRequestsType();
+        ArrayOfSourceRequestRequestsType sr = jaxbFactory
+                .createArrayOfSourceRequestRequestsType();
         sr.getSourceRequest().add(sourceRequestType);
 
-        SearchRequestType searchRequestType = jaxbFactory.createSearchRequestType();
+        SearchRequestType searchRequestType = jaxbFactory
+                .createSearchRequestType();
         searchRequestType.setAppID("5588C3ACE949315B3ECAADDA908611BDF5D8D5AA");
         searchRequestType.setRequests(sr);
         searchRequestType.setQuery("LegStar");
@@ -168,11 +187,14 @@ public class WebServiceInvokerTest extends TestCase {
 
     /**
      * Check the dispatcher caching mechanism.
+     * Have to disable this one. MSN does not like it anymore.
+     * 
      * @throws Exception when test fails
      */
-    public void testWebServiceInvokerReuse() throws Exception {
+    public void notestWebServiceInvokerReuse() throws Exception {
         /* Setup a configuration and instantiate action. */
-        WebServiceInvoker invoker = new WebServiceInvoker(MSNSearchJaxwsCases.getReflectConfig());
+        WebServiceInvoker invoker = new WebServiceInvoker(MSNSearchJaxwsCases
+                .getReflectConfig());
 
         int threadsSize = 5;
         Thread[] threads = new Thread[threadsSize];
@@ -195,7 +217,7 @@ public class WebServiceInvokerTest extends TestCase {
 
     /**
      * Class used for testing sharing invokers between threads.
-     *
+     * 
      */
     public class WebServiceInvokerRunnable implements Runnable {
 
@@ -216,8 +238,10 @@ public class WebServiceInvokerTest extends TestCase {
         public void run() {
             try {
                 String id = Thread.currentThread().getName();
-                SearchResponse searchResponse = _invoker.invoke(id, getSearchRequest());
-                assertTrue(searchResponse.getResponse().getResponses().getSourceResponse().size() > 0);
+                SearchResponse searchResponse = _invoker.invoke(id,
+                        getSearchRequest());
+                assertTrue(searchResponse.getResponse().getResponses()
+                        .getSourceResponse().size() > 0);
             } catch (Exception e) {
                 e.printStackTrace();
                 _exception = e;
@@ -232,6 +256,5 @@ public class WebServiceInvokerTest extends TestCase {
         }
 
     }
-
 
 }
