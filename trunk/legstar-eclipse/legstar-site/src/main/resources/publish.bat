6efc0cdbdcbfc0628b8b0081d4501ce1cd452572
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2011-03-08 13:36:07 +0000

    normalized legstar-site

--- trunk/legstar-eclipse/legstar-site/src/main/resources/publish.bat
+++ trunk/legstar-eclipse/legstar-site/src/main/resources/publish.bat
@@ -1,7 +1,7 @@
 java -jar "%ECLIPSE_HOME%/plugins/org.eclipse.equinox.launcher_1.1.0.v20100507.jar" ^
 -application org.eclipse.equinox.p2.publisher.UpdateSitePublisher ^
--metadataRepository file:/@LEGSTAR-LOCAL-SITE-LOCATION@ ^
--artifactRepository file:/@LEGSTAR-LOCAL-SITE-LOCATION@ ^
--source /@LEGSTAR-LOCAL-SITE-LOCATION@ ^
+-metadataRepository file:/${legstarPluginVersion} ^
+-artifactRepository file:/${legstarPluginVersion} ^
+-source /${legstarPluginVersion} ^
 -compress  ^
 -configs gtk.linux.x86
