5f4373b2b0713a40f07b7ea87e58f7d8f926d87d
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2011-03-15 10:33:54 +0000

    [maven-scm] copy for tag legstar-skin-1.0.4

--- legstar-eclipse/legstar-site/src/main/resources/publish.bat
+++
@@ -1,7 +0,0 @@
-java -jar "%ECLIPSE_HOME%/plugins/org.eclipse.equinox.launcher_1.1.0.v20100507.jar" ^
--application org.eclipse.equinox.p2.publisher.UpdateSitePublisher ^
--metadataRepository file:/${legstarPluginVersion} ^
--artifactRepository file:/${legstarPluginVersion} ^
--source /${legstarPluginVersion} ^
--compress  ^
--configs gtk.linux.x86
