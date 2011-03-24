fa1012ffb38656483a0966bf6fafda9710ec1fca
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2011-03-24 08:57:18 +0000

    Fixed a few minor issues

--- legstar-core/legstar-jaxbgen/src/test/java/com/legstar/test/coxb/CultureinfoCases.java
+++ legstar-core/legstar-jaxbgen/src/test/java/com/legstar/test/coxb/CultureinfoCases.java
@@ -12,6 +12,8 @@ package com.legstar.test.coxb;
 
 import java.math.BigDecimal;
 
+import junit.framework.TestCase;
+
 import com.legstar.coxb.host.HostData;
 import com.legstar.test.coxb.cultureinfo.CultureInfoParameters;
 import com.legstar.test.coxb.cultureinfo.CultureInfoReply;
@@ -19,13 +21,10 @@ import com.legstar.test.coxb.cultureinfo.GetInfo;
 import com.legstar.test.coxb.cultureinfo.ObjectFactory;
 import com.legstar.test.coxb.cultureinfo.ServerCultureInfo;
 
-import junit.framework.TestCase;
-
 /**
- * Provides data samples for testing throughout LegStar.  
+ * Provides data samples for testing throughout LegStar.
  */
 public class CultureinfoCases extends TestCase {
-
 
     /** Utility class. */
     private CultureinfoCases() {
@@ -35,49 +34,50 @@ public class CultureinfoCases extends TestCase {
     /**
      * @return a hexadecimal representation of host data.
      */
-    public static String getHostBytesHexRequestFr() { 
+    public static String getHostBytesHexRequestFr() {
 
         return
-        /* F R - f r                                                     275.36     */
+        /* F R - f r 275.36 */
         "c6d9608699404040404040404040404040404040404040404040404040404040000027536f";
     }
 
     /**
      * @return a hexadecimal representation of host data.
      */
-    public static String getHostBytesHexReplyFr() { 
+    public static String getHostBytesHexReplyFr() {
 
         /** Sample raw mainframe response sample. */
         return
-        /* €                                                     275.36   */
+        /* € 275.36 */
         "9f40404040404040404040404040404040404040404040404040404040404040"
-        /*  F r a n c e                                                    */
+        /* F r a n c e */
         + "c699819583854040404040404040404040404040404040404040404040404040"
-        /*  f r a n ç a i s                                                */
-        + "86998195488189a2404040404040404040404040404040404040404040404040"
-        /*  m a r d i   2 8   o c t o b r e   2 0 0 8   1 3   h   4 5   C E*/
+        /* f r a n ç a i s */
+        + "86998195e08189a2404040404040404040404040404040404040404040404040"
+        /* m a r d i 2 8 o c t o b r e 2 0 0 8 1 3 h 4 5 C E */
         + "948199848940f2f8409683a39682998540f2f0f0f840f1f3408840f4f540c3c5"
-        /*  2 7 5 , 3 6                                                    */
+        /* 2 7 5 , 3 6 */
         + "f2f7f56bf3f64040404040404040404040404040404040404040404040404040"
-        /*  f r - F R                                                      */
+        /* f r - F R */
         + "869960c6d9404040404040404040404040404040404040404040404040404040"
-        /*  F r a n c e                                                    */
+        /* F r a n c e */
         + "c699819583854040404040404040404040404040404040404040404040404040"
-        /*  f r a n ç a i s                                                */
-        + "86998195488189a2404040404040404040404040404040404040404040404040";
+        /* f r a n ç a i s */
+        + "86998195e08189a2404040404040404040404040404040404040404040404040";
     }
 
     /**
      * Because there is a variable part in the response (date/time), we only
      * check the fixed portion of the reply.
+     * 
      * @param hostBytes the host bytes to check
      */
     public static void checkHostBytesReplyFr(final byte[] hostBytes) {
         String response = HostData.toHexString(hostBytes);
-        assertEquals(response.substring(0, 191),
-                getHostBytesHexReplyFr().substring(0, 191));
-        assertEquals(response.substring(256, 511),
-                getHostBytesHexReplyFr().substring(256, 511));
+        assertEquals(response.substring(0, 191), getHostBytesHexReplyFr()
+                .substring(0, 191));
+        assertEquals(response.substring(256, 511), getHostBytesHexReplyFr()
+                .substring(256, 511));
     }
 
     /**
@@ -86,7 +86,8 @@ public class CultureinfoCases extends TestCase {
     public static GetInfo getJavaObjectRequestFr() {
         ObjectFactory of = new ObjectFactory();
         GetInfo getInfo = of.createGetInfo();
-        CultureInfoParameters cultureInfoParameters = of.createCultureInfoParameters();
+        CultureInfoParameters cultureInfoParameters = of
+                .createCultureInfoParameters();
         cultureInfoParameters.setCultureCode("fr-FR");
         cultureInfoParameters.setDecimalNumber(new BigDecimal("125645.62"));
         getInfo.setArg0(cultureInfoParameters);
