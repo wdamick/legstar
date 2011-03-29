bf6a9f8ed6ecdb4e053e9fc08d189649112dd493
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2011-03-29 16:07:21 +0000

    [maven-scm] copy for tag legstar-transport-1.5.0

--- legstar-core/legstar-jaxbgen/src/test/java/com/legstar/test/coxb/MSNSearch/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://schemas.microsoft.com/MSNSearch/2005/09/fex", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.legstar.test.coxb.MSNSearch;
