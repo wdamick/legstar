97be40c7e6f940853376b958961a9ea2166c902d
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2011-03-31 07:12:12 +0000

    [maven-release-plugin]  copy for tag legstar-distribution-1.5.0

--- legstar-core/legstar-jaxbgen/src/test/java/com/legstar/test/coxb/lsfileae/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://legstar.com/test/coxb/lsfileae", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.legstar.test.coxb.lsfileae;
