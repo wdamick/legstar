9b779928d95cd4d97ac38610c6551565801cb67b
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2011-07-23 05:09:27 +0000

    [maven-scm] copy for tag legstar-transport-1.5.1

--- legstar-core/legstar-jaxbgen/src/test/java/com/legstar/test/coxb/valuemix/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://legstar.com/test/coxb/valuemix", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.legstar.test.coxb.valuemix;
