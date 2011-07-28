f0775bf8973d67027f702a96e80f882942441268
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2011-07-28 14:19:00 +0000

    [maven-scm] copy for tag legstar-eclipse-1.5.1

--- legstar-core/legstar-jaxbgen/src/test/java/com/legstar/test/coxb/binnatus/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://legstar.com/test/coxb/binnatus", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.legstar.test.coxb.binnatus;
