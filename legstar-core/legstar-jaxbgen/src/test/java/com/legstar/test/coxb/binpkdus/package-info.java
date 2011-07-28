1566f7be6bab3aaa92e6da549a435ffba0e926bf
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2011-07-28 09:14:42 +0000

    [maven-scm] copy for tag legstar-cixsgen-1.5.1

--- legstar-core/legstar-jaxbgen/src/test/java/com/legstar/test/coxb/binpkdus/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://legstar.com/test/coxb/binpkdus", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.legstar.test.coxb.binpkdus;
