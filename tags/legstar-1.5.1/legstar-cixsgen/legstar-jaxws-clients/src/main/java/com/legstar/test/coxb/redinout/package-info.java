d0a2745b20538470bd690b82561042ef0a2af5c6
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2011-07-28 15:11:06 +0000

    Some checked out files

--- tags/legstar-1.5.1/legstar-cixsgen/legstar-jaxws-clients/src/main/java/com/legstar/test/coxb/redinout/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://legstar.com/test/coxb/redinout", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.legstar.test.coxb.redinout;
