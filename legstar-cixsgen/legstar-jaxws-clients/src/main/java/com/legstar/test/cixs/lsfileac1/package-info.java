851c4d7136fc77650e447959414898757eade44b
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2011-07-28 05:24:20 +0000

    [maven-scm] copy for tag legstar-cob2trans-1.5.1

--- legstar-cixsgen/legstar-jaxws-clients/src/main/java/com/legstar/test/cixs/lsfileac1/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://cixs.test.legstar.com/lsfileac1", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.legstar.test.cixs.lsfileac1;
--- legstar-cixsgen/legstar-jaxws-clients/src/main/java/com/legstar/test/coxb/redmulti/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://legstar.com/test/coxb/redmulti", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.legstar.test.coxb.redmulti;
