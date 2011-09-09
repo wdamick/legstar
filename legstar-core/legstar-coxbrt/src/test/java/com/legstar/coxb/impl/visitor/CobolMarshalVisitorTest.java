13c67a0aeb4eabfea8a0998c796d322048955a18
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2011-09-09 09:38:14 +0000

    Fixing Issue 156: Dynamic counters are not always automatically marshaled

--- legstar-core/legstar-coxbrt/src/test/java/com/legstar/coxb/impl/visitor/CobolMarshalVisitorTest.java
+++ legstar-core/legstar-coxbrt/src/test/java/com/legstar/coxb/impl/visitor/CobolMarshalVisitorTest.java
@@ -10,12 +10,16 @@
  ******************************************************************************/
 package com.legstar.coxb.impl.visitor;
 
+import java.math.BigDecimal;
+
 import junit.framework.TestCase;
 
 import com.legstar.coxb.convert.simple.CobolSimpleConverters;
 import com.legstar.coxb.host.HostData;
 import com.legstar.coxb.impl.reflect.CComplexReflectBinding;
 import com.legstar.test.coxb.coxb137.BoolPojo;
+import com.legstar.test.coxb.pojo156.jaxb.SomeClass;
+import com.legstar.test.coxb.pojo156.jaxb.SomeItem;
 
 /**
  * There are several marshaling tests elsewhere in this project. Here we focus
@@ -47,6 +51,61 @@ public class CobolMarshalVisitorTest extends TestCase {
         mv = new CobolMarshalVisitor(hostBytes, 0, new CobolSimpleConverters());
         ccem.accept(mv);
         assertEquals("0000000100000001", HostData.toHexString(hostBytes));
+
+    }
+
+    /**
+     * Tests that an implicit dynamic counter is marshaled correctly.
+     * 
+     * @throws Exception if test fails
+     */
+    public void testDynCountersPojo() throws Exception {
+        SomeClass someClass = new SomeClass();
+        for (int i = 0; i < 2; i++) {
+            SomeItem item = new SomeItem();
+            item.setAmount(new BigDecimal(125.23 * i));
+            item.setLabel("item" + i);
+            item.setNumber(i);
+            someClass.getItems().add(item);
+        }
+
+        CComplexReflectBinding ccem = new CComplexReflectBinding(
+                new com.legstar.test.coxb.pojo156.jaxb.ObjectFactory(),
+                someClass);
+        byte[] hostBytes = new byte[86];
+        CobolMarshalVisitor mv = new CobolMarshalVisitor(hostBytes, 0,
+                new CobolSimpleConverters());
+        ccem.accept(mv);
+        assertEquals(86, mv.getOffset());
+        assertEquals(
+                "00000002"
+                        + "000000000c89a38594f040404040404040404040404040404040404040404040404040404000000000"
+                        + "000012523c89a38594f140404040404040404040404040404040404040404040404040404000000001",
+                HostData.toHexString(hostBytes));
+    }
+
+    /**
+     * Tests that an explicit dynamic counter is marshaled correctly.
+     * 
+     * @throws Exception if test fails
+     */
+    public void testDynCountersCobol() throws Exception {
+        com.legstar.test.coxb.cob156.A parent = new com.legstar.test.coxb.cob156.A();
+        // Don't set the explicit B counter, it should be automatically valued
+        com.legstar.test.coxb.cob156.C child = new com.legstar.test.coxb.cob156.C();
+        for (int i = 0; i < 3; i++) {
+            child.getD().add(Integer.toString(i));
+        }
+        parent.setC(child);
+
+        CComplexReflectBinding ccem = new CComplexReflectBinding(
+                new com.legstar.test.coxb.cob156.ObjectFactory(), parent);
+        byte[] hostBytes = new byte[6];
+        CobolMarshalVisitor mv = new CobolMarshalVisitor(hostBytes, 0,
+                new CobolSimpleConverters());
+        ccem.accept(mv);
+        assertEquals(6, mv.getOffset());
+        assertEquals("f0f0f3f0f1f2", HostData.toHexString(hostBytes));
 
     }
 }
