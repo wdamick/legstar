a158aa7d832ae52f9711ad174a9079ad9ff78294
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2011-11-19 10:18:37 +0000

    Changed velocity version to 1.6.3 because it has sources properly distributed via maven. Also removed code that was not compatible with older versions of eclipse (of osgi actually)

--- trunk/legstar-eclipse/legstar-jaxwsgen-plugin/src/main/java/com/legstar/eclipse/plugin/jaxwsgen/wizards/Jaxws2CixsGeneratorWizardLauncher.java
+++ trunk/legstar-eclipse/legstar-jaxwsgen-plugin/src/main/java/com/legstar/eclipse/plugin/jaxwsgen/wizards/Jaxws2CixsGeneratorWizardLauncher.java
@@ -10,34 +10,31 @@
  ******************************************************************************/
 package com.legstar.eclipse.plugin.jaxwsgen.wizards;
 
-import java.util.Properties;
-
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.wizard.IWizard;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 
-import com.legstar.eclipse.plugin.cixscom.wizards
-.AbstractCixsGeneratorWizardLauncher;
+import com.legstar.eclipse.plugin.cixscom.wizards.AbstractCixsGeneratorWizardLauncher;
 import com.legstar.eclipse.plugin.cixscom.wizards.ICixsGeneratorWizardLauncher;
 import com.legstar.eclipse.plugin.jaxwsgen.Messages;
 
 /**
- * Implementation of a component generator which basically hands over
- * control to the Jaxws2Cixs wizard.
+ * Implementation of a component generator which basically hands over control to
+ * the Jaxws2Cixs wizard.
  */
-public class Jaxws2CixsGeneratorWizardLauncher
-extends AbstractCixsGeneratorWizardLauncher {
+public class Jaxws2CixsGeneratorWizardLauncher extends
+        AbstractCixsGeneratorWizardLauncher {
 
     /**
      * Get an instance of the wizard.
+     * 
      * @param mappingFile the mapping file
      * @return a new instance of the wizard
      * @throws CoreException if wizard cannot be instantiated
      */
-    protected IWizard getWizard(
-            final IFile mappingFile) throws CoreException {
+    protected IWizard getWizard(final IFile mappingFile) throws CoreException {
         return new Jaxws2CixsGeneratorWizard(mappingFile);
 
     }
@@ -50,15 +47,14 @@ extends AbstractCixsGeneratorWizardLauncher {
     /**
      * Register an OSGI component generation service that can be discovered
      * dynamically by other plugins (bundles).
+     * 
      * @param context the current bundle context
      * @return a registration service
      */
-    public static ServiceRegistration register(
-            final BundleContext context) {
+    public static ServiceRegistration register(final BundleContext context) {
         return context.registerService(
                 ICixsGeneratorWizardLauncher.class.getName(),
-                new Jaxws2CixsGeneratorWizardLauncher(),
-                new Properties());
+                new Jaxws2CixsGeneratorWizardLauncher(), null);
 
     }
 }
