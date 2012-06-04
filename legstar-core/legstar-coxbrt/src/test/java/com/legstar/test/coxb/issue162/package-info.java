eaf36a4dc6f8c2baeb02cae28bdeecff53b54c66
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2012-06-04 11:35:33 +0000

    [maven-scm] copy for tag legstar-cob2trans-1.5.2

--- legstar-core/legstar-coxbrt/src/test/java/com/legstar/test/coxb/issue162/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://coxb.test.legstar.com/issue162", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.legstar.test.coxb.issue162;
--- legstar-core/legstar-jaxbgen/src/test/java/com/legstar/test/coxb/rq071/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://creditstatus.customer.ibg/")
-package com.legstar.test.coxb.rq071;
