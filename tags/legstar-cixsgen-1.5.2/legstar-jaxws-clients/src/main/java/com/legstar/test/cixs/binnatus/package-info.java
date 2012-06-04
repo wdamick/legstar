3fcfe2421cbf1b4b80c02897c86325a3aa29eabe
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2012-06-04 14:02:10 +0000

    Rollback release

--- tags/legstar-cixsgen-1.5.2/legstar-jaxws-clients/src/main/java/com/legstar/test/cixs/binnatus/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://cixs.test.legstar.com/binnatus", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.legstar.test.cixs.binnatus;
