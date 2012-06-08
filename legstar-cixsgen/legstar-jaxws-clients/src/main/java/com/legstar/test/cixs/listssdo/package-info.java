b7497f5aa23cf999f7fe914cc8f74c0c153533b3
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2012-06-08 07:14:35 +0000

    [maven-release-plugin]  copy for tag legstar-distribution-1.5.2

--- legstar-cixsgen/legstar-jaxws-clients/src/main/java/com/legstar/test/cixs/listssdo/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://cixs.test.legstar.com/listssdo", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.legstar.test.cixs.listssdo;
--- legstar-core/legstar-jaxbgen/src/test/java/com/legstar/test/coxb/jvmquery/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://jvmquery.cases.test.xsdc.legstar.com/")
-package com.legstar.test.coxb.jvmquery;
