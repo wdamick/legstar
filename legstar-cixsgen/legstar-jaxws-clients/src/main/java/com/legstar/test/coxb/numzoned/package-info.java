064133f2ce31c3f64fa8c8f6c6ba0e8f988dfdb4
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2012-06-08 09:17:37 +0000

    [maven-scm] copy for tag legstar-eclipse-1.5.2

--- legstar-cixsgen/legstar-jaxws-clients/src/main/java/com/legstar/test/coxb/numzoned/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://legstar.com/test/coxb/numzoned", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.legstar.test.coxb.numzoned;
