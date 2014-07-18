159b3fabf5cf4f27fa08b4ffb97f7abfe3202354
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2014-07-18 08:37:30 +0000

    [maven-release-plugin]  copy for tag legstar-core-1.5.10

--- legstar-core/legstar-jaxbgen/src/test/java/com/legstar/test/coxb/fixarcom/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://legstar.com/test/coxb/fixarcom", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.legstar.test.coxb.fixarcom;
