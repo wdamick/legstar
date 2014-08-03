4a96aa45541a4694b9434d042f5b95694ad8970e
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2014-08-03 13:56:11 +0000

    [maven-scm] copy for tag legstar-transport-1.5.3

--- legstar-core/legstar-coxbrt/src/test/java/com/legstar/test/coxb/issue161/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://coxb.test.legstar.com/issue161", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.legstar.test.coxb.issue161;
