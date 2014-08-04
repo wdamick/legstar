7c157d70234f9073e79a83692beaff006f67bd8c
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2014-08-04 08:28:11 +0000

    [maven-scm] copy for tag legstar-cixsgen-1.5.3

--- legstar-core/legstar-jaxbgen/src/test/java/com/legstar/test/coxb/lsfileal/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://legstar.com/test/coxb/lsfileal", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.legstar.test.coxb.lsfileal;
