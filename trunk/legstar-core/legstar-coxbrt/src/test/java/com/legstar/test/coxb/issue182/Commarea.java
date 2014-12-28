6932e0092ba9f3ca3e617267ea565c2951250f66
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2014-12-28 11:56:14 +0000

    Fixes issue 182.

---
+++ trunk/legstar-core/legstar-coxbrt/src/test/java/com/legstar/test/coxb/issue182/Commarea.java
@@ -0,0 +1,93 @@
+
+package com.legstar.test.coxb.issue182;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import com.legstar.coxb.CobolElement;
+import com.legstar.coxb.CobolType;
+
+
+/**
+ * <p>Java class for Commarea complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="Commarea">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="aBinary" maxOccurs="2" minOccurs="2">
+ *           &lt;simpleType>
+ *             &lt;restriction base="{http://www.w3.org/2001/XMLSchema}hexBinary">
+ *               &lt;maxLength value="4"/>
+ *             &lt;/restriction>
+ *           &lt;/simpleType>
+ *         &lt;/element>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "Commarea", propOrder = {
+    "aBinary"
+})
+public class Commarea
+    implements Serializable
+{
+
+    private final static long serialVersionUID = 1L;
+    @XmlElement(required = true, type = String.class)
+    @XmlJavaTypeAdapter(HexBinaryAdapter.class)
+    @CobolElement(cobolName = "A-BINARY", type = CobolType.OCTET_STREAM_ITEM, levelNumber = 5, minOccurs = 2, maxOccurs = 2, picture = "X(4)", srceLine = 2)
+    protected List<byte[]> aBinary;
+
+    /**
+     * Gets the value of the aBinary property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the aBinary property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getABinary().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link String }
+     * 
+     * 
+     */
+    public List<byte[]> getABinary() {
+        if (aBinary == null) {
+            aBinary = new ArrayList<byte[]>();
+        }
+        return this.aBinary;
+    }
+
+    public boolean isSetABinary() {
+        return ((this.aBinary!= null)&&(!this.aBinary.isEmpty()));
+    }
+
+    public void unsetABinary() {
+        this.aBinary = null;
+    }
+
+}
