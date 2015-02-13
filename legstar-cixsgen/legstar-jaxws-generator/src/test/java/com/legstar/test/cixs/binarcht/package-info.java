b83524ea57341fce9ead65cf65c2941e6ffcdf59
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2015-02-13 16:44:31 +0000

    [maven-release-plugin]  copy for tag legstar-core-1.6.0

--- legstar-cixsgen/legstar-jaxws-generator/src/test/java/com/legstar/test/cixs/binarcht/package-info.java
+++
@@ -1,3 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://cixs.test.legstar.com/binarcht",
-    elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.legstar.test.cixs.binarcht;
