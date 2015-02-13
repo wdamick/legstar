9b87de8ead68b8b0a2373adac09d2c381106ef74
fady.moussallam <fady.moussallam@122eaf1c-c83d-11dd-a8b6-f5aca7b582bb>
2015-02-13 16:28:39 +0000

    Updates copyright notices

--- legstar-core/legstar-coxbrt/src/test/java/com/legstar/test/coxb/issue161/package-info.java
+++ legstar-core/legstar-coxbrt/src/test/java/com/legstar/test/coxb/issue161/package-info.java
@@ -1,2 +1,12 @@
+/*******************************************************************************
+ * Copyright (c) 2015 LegSem.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v2.1
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     LegSem - initial API and implementation
+ ******************************************************************************/
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://coxb.test.legstar.com/issue161", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package com.legstar.test.coxb.issue161;
